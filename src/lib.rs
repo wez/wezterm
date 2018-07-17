3a31975f06c074cdbebb3c62afe9b5451bbcedc2
Wez Furlong <wez@wezfurlong.org>
2018-07-17 11:09:59 -0700

    Add terminal capabilities struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate failure;
 extern crate palette;
+extern crate semver;
 extern crate serde;
 extern crate terminfo;
 #[macro_use]
@@ -9,7 +10,10 @@ extern crate num;
 extern crate vte;
 #[macro_use]
 extern crate num_derive;
+#[macro_use]
+extern crate derive_builder;
 
+pub mod caps;
 pub mod cell;
 pub mod color;
 pub mod escape;
