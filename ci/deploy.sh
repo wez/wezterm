c57ab9a9421624d04da3c92c5396d41c6ee54ed7
Wez Furlong <wez@wezfurlong.org>
2019-03-23 23:59:08 -0700

    Embed tag in the zip file name

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -x
 
+TRAVIS_TAG=${TRAVIS_TAG:-$(git describe --tags)}
+TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y%m%d-%H%M%S')-$(git log --format=%h -1)}
+
 ./install.sh
 
 HERE=$(pwd)
 
 case $OSTYPE in
   darwin*)
-    (cd $HOME/Applications && zip -r $HERE/WezTerm-macOS.zip WezTerm.app)
+    (cd $HOME/Applications && zip -r $HERE/WezTerm-macOS-$TRAVIS_TAG.zip WezTerm.app)
     ;;
   *)
     ;;
