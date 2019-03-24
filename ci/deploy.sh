f7c37432470ab923be42c41055dbd150112c053a
Wez Furlong <wez@wezfurlong.org>
2019-03-24 13:55:08 -0700

    maybe teach appveyor how to deploy windows builds

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 set -x
 
-TRAVIS_TAG=${TRAVIS_TAG:-$(git describe --tags)}
-TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y%m%d-%H%M%S')-$(git log --format=%h -1)}
+if [[ "$TRAVIS" != "" ]] ; then
+  DEPLOY_ENV_TYPE="travis"
+  TAG_NAME=$TRAVIS_TAG
+elif [[ "$APPVEYOR" != "" ]] ; then
+  DEPLOY_ENV_TYPE="appveyor"
+  TAG_NAME=$APPVEYOR_REPO_TAG_NAME
+else
+  DEPLOY_ENV_TYPE="adhoc"
+fi
 
-bash -x ./install.sh
+TAG_NAME=${TAG_NAME:-$(git describe --tags)}
+TAG_NAME=${TAG_NAME:-$(date +'%Y%m%d-%H%M%S')-$(git log --format=%h -1)}
 
 HERE=$(pwd)
 
 case $OSTYPE in
   darwin*)
-    (cd $HOME/Applications && zip -r $HERE/WezTerm-macOS-$TRAVIS_TAG.zip WezTerm.app)
+    zipdir=WezTerm-macos-$DEPLOY_ENV_TYPE-$TAG_NAME
+    rm -rf $zipdir $zipdir.zip
+    mkdir $zipdir
+    cp -r assets/macos/WezTerm.app $zipdir/
+    cp target/release/wezterm $zipdir/WezTerm.app
+    zip -r $zipdir.zip $zipdir
     ;;
   msys)
-    zipdir=WezTerm-windows-$TRAVIS_TAG
-    mkdir zipdir
+    zipdir=WezTerm-windows-$DEPLOY_ENV_TYPE-$TAG_NAME
+    rm -rf $zipdir $zipdir.zip
+    mkdir $zipdir
     cp target/release/wezterm.exe target/release/wezterm.pdb $zipdir
     7z a -tzip $zipdir.zip $zipdir
     ;;
