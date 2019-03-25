cd93f287fdce6ba3e86a15ea5acf531d68099d08
Wez Furlong <wez@wezfurlong.org>
2019-03-24 17:49:28 -0700

    Ask travis to build and publish a .deb to a github release

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -32,6 +32,32 @@ case $OSTYPE in
     cp target/release/wezterm.exe target/release/wezterm.pdb $zipdir
     7z a -tzip $zipdir.zip $zipdir
     ;;
+  linux-gnu)
+    case `lsb_release -ds` in
+      Ubuntu*|Debian*)
+        rm -rf pkg
+        mkdir -p pkg/debian/usr/bin pkg/debian/DEBIAN
+        cat > pkg/debian/DEBIAN/control <<EOF
+Package: wezterm
+Version: ${TAG_NAME}
+Architecture: amd64
+Maintainer: Wez Furlong <wez@wezfurlong.org>
+Section: utils
+Priority: optional
+Homepage: https://github.com/wez/wezterm
+Description: Wez's Terminal Emulator.
+ wezterm is a terminal emulator with support for modern features
+ such as fonts with ligatures, hyperlinks, tabs and multiple
+ windows.
+Depends: libc6, libegl-mesa0, libxcb-icccm4, libxcb-ewmh2, libxcb-keysyms1, libxcb-xkb1, libxkbcommon0, libxkbcommon-x11-0, libfontconfig1, xdg-utils, libxcb-render0, libxcb-shape0, libx11-6, libegl1
+EOF
+        cp target/release/wezterm pkg/debian/usr/bin
+        fakeroot dpkg-deb --build pkg/debian wezterm-$TAG_NAME.deb
+        rm -rf pkg
+      ;;
+    esac
+
+    ;;
   *)
     ;;
 esac
