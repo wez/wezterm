a7c4eb430d1a944ac2539d18742989b96abd5204
Wez Furlong <wez@wezfurlong.org>
2019-09-28 12:45:00 -0700

    export key map code from guicommon

--- src/frontend/guicommon/host.rs
+++ src/frontend/guicommon/host.rs
@@ -90,89 +90,95 @@ fn trickle_paste(tab_id: TabId, text: String) {
     schedule_next_paste(&paste);
 }
 
-type KeyMap = HashMap<(KeyCode, KeyModifiers), KeyAssignment>;
+pub struct KeyMap(HashMap<(KeyCode, KeyModifiers), KeyAssignment>);
 
-fn key_bindings() -> KeyMap {
-    let mux = Mux::get().unwrap();
-    let mut map = mux
-        .config()
-        .key_bindings()
-        .expect("keys section of config to be valid");
+impl KeyMap {
+    pub fn new() -> Self {
+        let mux = Mux::get().unwrap();
+        let mut map = mux
+            .config()
+            .key_bindings()
+            .expect("keys section of config to be valid");
 
-    macro_rules! m {
-        ($([$mod:expr, $code:expr, $action:expr]),* $(,)?) => {
-            $(
-            map.entry(($code, $mod)).or_insert($action);
-            )*
+        macro_rules! m {
+            ($([$mod:expr, $code:expr, $action:expr]),* $(,)?) => {
+                $(
+                map.entry(($code, $mod)).or_insert($action);
+                )*
+            };
         };
-    };
 
-    use KeyAssignment::*;
+        use KeyAssignment::*;
 
-    // Apply the default bindings; if the user has already mapped
-    // a given entry then that will take precedence.
-    m!(
-        // Clipboard
-        [KeyModifiers::SUPER, KeyCode::Char('c'), Copy],
-        [KeyModifiers::SUPER, KeyCode::Char('v'), Paste],
-        [KeyModifiers::SHIFT, KeyCode::Insert, Paste],
-        // Window management
-        [KeyModifiers::SUPER, KeyCode::Char('m'), Hide],
-        [KeyModifiers::SUPER, KeyCode::Char('n'), SpawnWindow],
-        [KeyModifiers::ALT, KeyCode::Char('\n'), ToggleFullScreen],
-        [KeyModifiers::ALT, KeyCode::Char('\r'), ToggleFullScreen],
-        [KeyModifiers::ALT, KeyCode::Enter, ToggleFullScreen],
-        // Font size manipulation
-        [KeyModifiers::SUPER, KeyCode::Char('-'), DecreaseFontSize],
-        [KeyModifiers::CTRL, KeyCode::Char('-'), DecreaseFontSize],
-        [KeyModifiers::SUPER, KeyCode::Char('='), IncreaseFontSize],
-        [KeyModifiers::CTRL, KeyCode::Char('='), IncreaseFontSize],
-        [KeyModifiers::SUPER, KeyCode::Char('0'), ResetFontSize],
-        [KeyModifiers::CTRL, KeyCode::Char('0'), ResetFontSize],
-        // Tab navigation and management
-        [
-            KeyModifiers::SUPER,
-            KeyCode::Char('t'),
-            SpawnTab(SpawnTabDomain::DefaultDomain)
-        ],
-        [
-            KeyModifiers::SUPER | KeyModifiers::SHIFT,
-            KeyCode::Char('T'),
-            SpawnTab(SpawnTabDomain::CurrentTabDomain)
-        ],
-        [KeyModifiers::SUPER, KeyCode::Char('w'), CloseCurrentTab],
-        [KeyModifiers::SUPER, KeyCode::Char('1'), ActivateTab(0)],
-        [KeyModifiers::SUPER, KeyCode::Char('2'), ActivateTab(1)],
-        [KeyModifiers::SUPER, KeyCode::Char('3'), ActivateTab(2)],
-        [KeyModifiers::SUPER, KeyCode::Char('4'), ActivateTab(3)],
-        [KeyModifiers::SUPER, KeyCode::Char('5'), ActivateTab(4)],
-        [KeyModifiers::SUPER, KeyCode::Char('6'), ActivateTab(5)],
-        [KeyModifiers::SUPER, KeyCode::Char('7'), ActivateTab(6)],
-        [KeyModifiers::SUPER, KeyCode::Char('8'), ActivateTab(7)],
-        [KeyModifiers::SUPER, KeyCode::Char('9'), ActivateTab(8)],
-        [
-            KeyModifiers::SUPER | KeyModifiers::SHIFT,
-            KeyCode::Char('['),
-            ActivateTabRelative(-1)
-        ],
-        [
-            KeyModifiers::SUPER | KeyModifiers::SHIFT,
-            KeyCode::Char('{'),
-            ActivateTabRelative(-1)
-        ],
-        [
-            KeyModifiers::SUPER | KeyModifiers::SHIFT,
-            KeyCode::Char(']'),
-            ActivateTabRelative(1)
-        ],
-        [
-            KeyModifiers::SUPER | KeyModifiers::SHIFT,
-            KeyCode::Char('}'),
-            ActivateTabRelative(1)
-        ],
-    );
+        // Apply the default bindings; if the user has already mapped
+        // a given entry then that will take precedence.
+        m!(
+            // Clipboard
+            [KeyModifiers::SUPER, KeyCode::Char('c'), Copy],
+            [KeyModifiers::SUPER, KeyCode::Char('v'), Paste],
+            [KeyModifiers::SHIFT, KeyCode::Insert, Paste],
+            // Window management
+            [KeyModifiers::SUPER, KeyCode::Char('m'), Hide],
+            [KeyModifiers::SUPER, KeyCode::Char('n'), SpawnWindow],
+            [KeyModifiers::ALT, KeyCode::Char('\n'), ToggleFullScreen],
+            [KeyModifiers::ALT, KeyCode::Char('\r'), ToggleFullScreen],
+            [KeyModifiers::ALT, KeyCode::Enter, ToggleFullScreen],
+            // Font size manipulation
+            [KeyModifiers::SUPER, KeyCode::Char('-'), DecreaseFontSize],
+            [KeyModifiers::CTRL, KeyCode::Char('-'), DecreaseFontSize],
+            [KeyModifiers::SUPER, KeyCode::Char('='), IncreaseFontSize],
+            [KeyModifiers::CTRL, KeyCode::Char('='), IncreaseFontSize],
+            [KeyModifiers::SUPER, KeyCode::Char('0'), ResetFontSize],
+            [KeyModifiers::CTRL, KeyCode::Char('0'), ResetFontSize],
+            // Tab navigation and management
+            [
+                KeyModifiers::SUPER,
+                KeyCode::Char('t'),
+                SpawnTab(SpawnTabDomain::DefaultDomain)
+            ],
+            [
+                KeyModifiers::SUPER | KeyModifiers::SHIFT,
+                KeyCode::Char('T'),
+                SpawnTab(SpawnTabDomain::CurrentTabDomain)
+            ],
+            [KeyModifiers::SUPER, KeyCode::Char('w'), CloseCurrentTab],
+            [KeyModifiers::SUPER, KeyCode::Char('1'), ActivateTab(0)],
+            [KeyModifiers::SUPER, KeyCode::Char('2'), ActivateTab(1)],
+            [KeyModifiers::SUPER, KeyCode::Char('3'), ActivateTab(2)],
+            [KeyModifiers::SUPER, KeyCode::Char('4'), ActivateTab(3)],
+            [KeyModifiers::SUPER, KeyCode::Char('5'), ActivateTab(4)],
+            [KeyModifiers::SUPER, KeyCode::Char('6'), ActivateTab(5)],
+            [KeyModifiers::SUPER, KeyCode::Char('7'), ActivateTab(6)],
+            [KeyModifiers::SUPER, KeyCode::Char('8'), ActivateTab(7)],
+            [KeyModifiers::SUPER, KeyCode::Char('9'), ActivateTab(8)],
+            [
+                KeyModifiers::SUPER | KeyModifiers::SHIFT,
+                KeyCode::Char('['),
+                ActivateTabRelative(-1)
+            ],
+            [
+                KeyModifiers::SUPER | KeyModifiers::SHIFT,
+                KeyCode::Char('{'),
+                ActivateTabRelative(-1)
+            ],
+            [
+                KeyModifiers::SUPER | KeyModifiers::SHIFT,
+                KeyCode::Char(']'),
+                ActivateTabRelative(1)
+            ],
+            [
+                KeyModifiers::SUPER | KeyModifiers::SHIFT,
+                KeyCode::Char('}'),
+                ActivateTabRelative(1)
+            ],
+        );
 
-    map
+        Self(map)
+    }
+
+    pub fn lookup(&self, key: KeyCode, mods: KeyModifiers) -> Option<KeyAssignment> {
+        self.0.get(&(key, mods)).cloned()
+    }
 }
 
 impl<H: HostHelper> HostImpl<H> {
@@ -180,7 +186,7 @@ impl<H: HostHelper> HostImpl<H> {
         Self {
             helper,
             clipboard: Arc::new(SystemClipboard::new()),
-            keys: key_bindings(),
+            keys: KeyMap::new(),
         }
     }
 
@@ -252,7 +258,7 @@ impl<H: HostHelper> HostImpl<H> {
         mods: KeyModifiers,
         key: KeyCode,
     ) -> Result<bool, Error> {
-        if let Some(assignment) = self.keys.get(&(key, mods)).cloned() {
+        if let Some(assignment) = self.keys.lookup(key, mods) {
             self.perform_key_assignment(tab, &assignment)?;
             Ok(true)
         } else {
