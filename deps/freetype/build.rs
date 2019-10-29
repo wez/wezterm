76571eb68e971e65575db010453960689ff2cce0
Wez Furlong <wez@wezfurlong.org>
2019-10-29 08:43:19 -0700

    build libz, libpng, freetype, harfbuzz without cmake
    use the cc crate to compile them directly

--- deps/freetype/build.rs
+++ deps/freetype/build.rs
@@ -1,6 +1,5 @@
-use cmake::Config;
-use fs_extra;
 use std::env;
+use std::fs;
 use std::path::{Path, PathBuf};
 
 fn zlib() {
@@ -8,48 +7,33 @@ fn zlib() {
         git_submodule_update();
     }
 
-    // The out-of-source build for zlib unfortunately modifies some of
-    // the sources, leaving the repo with a dirty status.  Let's take
-    // a copy of the sources so that we don't trigger this.
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let src_dir = out_dir.join("zlib-src");
-    if src_dir.exists() {
-        fs_extra::remove_items(&vec![&src_dir]).expect("failed to remove zlib-src");
-    }
-    std::fs::create_dir(&src_dir).expect("failed to create zlib-src");
-    fs_extra::copy_items(&vec!["zlib"], &src_dir, &fs_extra::dir::CopyOptions::new())
-        .expect("failed to copy zlib to zlib-src");
-
-    let mut config = Config::new(src_dir.join("zlib"));
-    let dst = config.profile("Release").build();
-    emit_libdirs(&dst);
-    if cfg!(unix) {
-        println!("cargo:rustc-link-lib=static=z");
-    } else {
-        println!("cargo:rustc-link-lib=static=zlibstatic");
-    }
-}
-
-fn emit_libdirs(p: &Path) {
-    for d in &["lib64", "lib"] {
-        let libdir = p.join(d);
-        if libdir.is_dir() {
-            println!("cargo:rustc-link-search=native={}", libdir.display());
-        }
-    }
-}
-
-fn libpath(p: &Path, name: &str) -> PathBuf {
-    for d in &["lib64", "lib"] {
-        for n in &[format!("lib{}.a", name), format!("{}.lib", name)] {
-            let libname = p.join(d).join(n);
-            if libname.is_file() {
-                return libname;
-            }
-        }
-    }
-    panic!("did not find {} in {}", name, p.display());
+    let mut cfg = cc::Build::new();
+    let build_dir = out_dir.join("zlib-build");
+    fs::create_dir_all(&build_dir).unwrap();
+    cfg.out_dir(&build_dir);
+    cfg.file("zlib/adler32.c")
+        .file("zlib/compress.c")
+        .file("zlib/crc32.c")
+        .file("zlib/deflate.c")
+        .file("zlib/gzclose.c")
+        .file("zlib/gzlib.c")
+        .file("zlib/gzread.c")
+        .file("zlib/gzwrite.c")
+        .file("zlib/inflate.c")
+        .file("zlib/infback.c")
+        .file("zlib/inftrees.c")
+        .file("zlib/inffast.c")
+        .file("zlib/trees.c")
+        .file("zlib/uncompr.c")
+        .file("zlib/zutil.c");
+    cfg.include("zlib");
+    cfg.define("HAVE_SYS_TYPES_H", None);
+    cfg.define("HAVE_STDINT_H", None);
+    cfg.define("HAVE_STDDEF_H", None);
+    cfg.define("_LARGEFILE64_SOURCE", Some("1"));
+    cfg.compile("z");
 }
 
 fn libpng() {
@@ -57,14 +41,36 @@ fn libpng() {
         git_submodule_update();
     }
 
-    let mut config = Config::new("libpng");
-    let dst = config.profile("Release").build();
-    emit_libdirs(&dst);
-    if cfg!(unix) {
-        println!("cargo:rustc-link-lib=static=png");
-    } else {
-        println!("cargo:rustc-link-lib=static=libpng16_static");
-    }
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let mut cfg = cc::Build::new();
+    let build_dir = out_dir.join("png-build");
+    fs::create_dir_all(&build_dir).unwrap();
+    cfg.out_dir(&build_dir);
+
+    cfg.file("libpng/png.c")
+        .file("libpng/pngerror.c")
+        .file("libpng/pngget.c")
+        .file("libpng/pngmem.c")
+        .file("libpng/pngpread.c")
+        .file("libpng/pngread.c")
+        .file("libpng/pngrio.c")
+        .file("libpng/pngrtran.c")
+        .file("libpng/pngrutil.c")
+        .file("libpng/pngset.c")
+        .file("libpng/pngtest.c")
+        .file("libpng/pngtrans.c")
+        .file("libpng/pngwio.c")
+        .file("libpng/pngwrite.c")
+        .file("libpng/pngwtran.c")
+        .file("libpng/pngwutil.c");
+
+    cfg.include("libpng");
+    cfg.define("HAVE_SYS_TYPES_H", None);
+    cfg.define("HAVE_STDINT_H", None);
+    cfg.define("HAVE_STDDEF_H", None);
+    cfg.define("_LARGEFILE64_SOURCE", Some("1"));
+    cfg.compile("png");
 }
 
 fn freetype() {
@@ -72,17 +78,114 @@ fn freetype() {
         git_submodule_update();
     }
 
-    let mut config = Config::new("freetype2");
-    let dst = config
-        .define("FT_WITH_PNG", "ON")
-        .define("CMAKE_DISABLE_FIND_PACKAGE_BZip2", "TRUE")
-        .profile("Release")
-        .build();
-    emit_libdirs(&dst);
-    println!("cargo:rustc-link-lib=static=freetype");
-    emit_libdirs(Path::new("/usr"));
-    println!("cargo:include={}/include/freetype2", dst.display());
-    println!("cargo:lib={}", libpath(&dst, "freetype").display());
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let mut cfg = cc::Build::new();
+    let build_dir = out_dir.join("freetype-build");
+    fs::create_dir_all(&build_dir).unwrap();
+    cfg.out_dir(&build_dir);
+
+    fs::create_dir_all(build_dir.join("freetype2/include/freetype/config")).unwrap();
+    cfg.include(format!("{}/freetype2/include", build_dir.display()));
+    cfg.include("freetype2/include");
+    cfg.define("FT2_BUILD_LIBRARY", None);
+
+    let target = env::var("TARGET").unwrap();
+    let ftconfig = if !target.contains("windows") {
+        fs::read_to_string("freetype2/builds/unix/ftconfig.in")
+            .unwrap()
+            .replace("#undef HAVE_UNISTD_H", "#define HAVE_UNISTD_H 1")
+            .replace("#undef HAVE_FCNTL_H", "#define HAVE_FCNTL_H 1")
+            .replace("#undef HAVE_STDINT_H", "#define HAVE_STDINT_H 1")
+            .replace("/undef", "#undef")
+    } else {
+        fs::read_to_string("freetype2/include/freetype/config/ftconfig.h").unwrap()
+    };
+
+    fs::write(
+        build_dir.join("freetype2/include/freetype/config/ftconfig.h"),
+        ftconfig,
+    )
+    .unwrap();
+
+    fs::write(
+        build_dir.join("freetype2/include/freetype/config/ftoption.h"),
+        fs::read_to_string("freetype2/include/freetype/config/ftoption.h")
+            .unwrap()
+            .replace(
+                "/* #define FT_CONFIG_OPTION_SYSTEM_ZLIB */",
+                "#define FT_CONFIG_OPTION_SYSTEM_ZLIB",
+            )
+            .replace(
+                "/* #define FT_CONFIG_OPTION_USE_PNG */",
+                "#define FT_CONFIG_OPTION_USE_PNG",
+            ),
+    )
+    .unwrap();
+
+    for f in [
+        "autofit/autofit.c",
+        "base/ftbase.c",
+        "base/ftbbox.c",
+        "base/ftbdf.c",
+        "base/ftbitmap.c",
+        "base/ftcid.c",
+        "base/ftfstype.c",
+        "base/ftgasp.c",
+        "base/ftglyph.c",
+        "base/ftgxval.c",
+        "base/ftinit.c",
+        "base/ftmm.c",
+        "base/ftotval.c",
+        "base/ftpatent.c",
+        "base/ftpfr.c",
+        "base/ftstroke.c",
+        "base/ftsynth.c",
+        "base/ftsystem.c",
+        "base/fttype1.c",
+        "base/ftwinfnt.c",
+        "bdf/bdf.c",
+        "bzip2/ftbzip2.c",
+        "cache/ftcache.c",
+        "cff/cff.c",
+        "cid/type1cid.c",
+        "gzip/ftgzip.c",
+        "lzw/ftlzw.c",
+        "pcf/pcf.c",
+        "pfr/pfr.c",
+        "psaux/psaux.c",
+        "pshinter/pshinter.c",
+        "psnames/psnames.c",
+        "raster/raster.c",
+        "sfnt/sfnt.c",
+        "smooth/smooth.c",
+        "truetype/truetype.c",
+        "type1/type1.c",
+        "type42/type42.c",
+        "winfonts/winfnt.c",
+    ]
+    .iter()
+    {
+        cfg.file(format!("freetype2/src/{}", f));
+    }
+
+    if target.contains("windows") {
+        cfg.file("freetype2/builds/windows/ftdebug.c")
+            .file("freetype2/src/base/ftver.rc");
+    } else {
+        cfg.file("freetype2/src/base/ftdebug.c");
+    }
+
+    cfg.compile("freetype");
+
+    // These cause DEP_FREETYPE_INCLUDE and DEP_FREETYPE_LIB to be
+    // defined in the harfbuzz/build.rs
+    println!(
+        "cargo:include={}/include/freetype2;{}/freetype2/include",
+        build_dir.display(),
+        std::env::current_dir().unwrap().display()
+    );
+    println!("cargo:lib={}", build_dir.display());
 }
 
 fn git_submodule_update() {
--- deps/harfbuzz/build.rs
+++ deps/harfbuzz/build.rs
@@ -1,51 +1,127 @@
-use cmake::Config;
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn harfbuzz() {
+    use std::fs;
+
     if !Path::new("harfbuzz/.git").exists() {
         git_submodule_update();
     }
 
-    let mut config = Config::new("harfbuzz");
-    for (key, value) in std::env::vars() {
-        println!("{}: {}", key, value);
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    let mut cfg = cc::Build::new();
+    cfg.warnings(false);
+    cfg.cpp(false);
+    cfg.flag_if_supported("-fno-rtti");
+    cfg.flag_if_supported("-fno-exceptions");
+    cfg.flag_if_supported("-fno-threadsafe-statics");
+    cfg.flag_if_supported("-std=c++11");
+
+    let build_dir = out_dir.join("harfbuzz-build");
+    fs::create_dir_all(&build_dir).unwrap();
+    cfg.out_dir(&build_dir);
+
+    let target = env::var("TARGET").unwrap();
+
+    for f in [
+        "hb-aat-layout.cc",
+        "hb-aat-map.cc",
+        "hb-blob.cc",
+        "hb-buffer-serialize.cc",
+        "hb-buffer.cc",
+        "hb-common.cc",
+        "hb-face.cc",
+        "hb-fallback-shape.cc",
+        "hb-font.cc",
+        "hb-ft.cc",
+        // "hb-graphite2.cc",
+        // "hb-glib.cc",
+        // "hb-gobject-structs.cc",
+        // "hb-icu.cc",
+        "hb-map.cc",
+        "hb-ot-cff1-table.cc",
+        "hb-ot-cff2-table.cc",
+        "hb-ot-color.cc",
+        "hb-ot-face.cc",
+        "hb-ot-font.cc",
+        "hb-ot-layout.cc",
+        "hb-ot-map.cc",
+        "hb-ot-math.cc",
+        "hb-ot-name-language.cc",
+        "hb-ot-name.cc",
+        "hb-ot-shape-complex-arabic.cc",
+        "hb-ot-shape-complex-default.cc",
+        "hb-ot-shape-complex-hangul.cc",
+        "hb-ot-shape-complex-hebrew.cc",
+        "hb-ot-shape-complex-indic-table.cc",
+        "hb-ot-shape-complex-indic.cc",
+        "hb-ot-shape-complex-khmer.cc",
+        "hb-ot-shape-complex-myanmar.cc",
+        "hb-ot-shape-complex-thai.cc",
+        "hb-ot-shape-complex-use-table.cc",
+        "hb-ot-shape-complex-use.cc",
+        "hb-ot-shape-complex-vowel-constraints.cc",
+        "hb-ot-shape-fallback.cc",
+        "hb-ot-shape-normalize.cc",
+        "hb-ot-shape.cc",
+        "hb-ot-tag.cc",
+        "hb-ot-var.cc",
+        "hb-set.cc",
+        "hb-shape-plan.cc",
+        "hb-shape.cc",
+        "hb-shaper.cc",
+        "hb-static.cc",
+        "hb-subset-cff-common.cc",
+        "hb-subset-cff1.cc",
+        "hb-subset-cff2.cc",
+        "hb-subset-glyf.cc",
+        "hb-subset-input.cc",
+        "hb-subset-plan.cc",
+        "hb-subset.cc",
+        "hb-ucdn.cc",
+        "hb-unicode.cc",
+        // "hb-warning.cc",
+        "hb-ucdn/ucdn.c",
+    ]
+    .iter()
+    {
+        cfg.file(format!("harfbuzz/src/{}", f));
     }
 
-    let ft_outdir = std::env::var("DEP_FREETYPE_OUTDIR").unwrap();
+    cfg.define("HAVE_FALLBACK", None);
+    cfg.define("HAVE_UCDN", None);
+    cfg.include("harfbuzz/src/hb-ucdn");
 
-    let dst = config
-        .env("CMAKE_PREFIX_PATH", &ft_outdir)
-        .cxxflag("-DHB_NO_PRAGMA_GCC_DIAGNOSTIC_ERROR")
-        .define("HB_HAVE_FREETYPE", "ON")
-        .define("HB_BUILD_TESTS", "OFF")
-        .define(
-            "FREETYPE_LIBRARY",
-            std::env::var("DEP_FREETYPE_LIB").unwrap(),
-        )
-        .define(
-            "FREETYPE_INCLUDE_DIR_ft2build",
-            std::env::var("DEP_FREETYPE_INCLUDE").unwrap(),
-        )
-        .define(
-            "FREETYPE_INCLUDE_DIR_freetype2",
-            std::env::var("DEP_FREETYPE_INCLUDE").unwrap(),
-        )
-        .profile("Release")
-        .build();
-    emit_libdirs(Path::new(&ft_outdir));
-    emit_libdirs(&dst);
-    emit_libdirs(Path::new("/usr"));
-    println!("cargo:rustc-link-lib=static=harfbuzz");
-}
-
-fn emit_libdirs(p: &Path) {
-    for d in &["lib64", "lib"] {
-        let libdir = p.join(d);
-        if libdir.is_dir() {
-            println!("cargo:rustc-link-search=native={}", libdir.display());
-        }
+    if !target.contains("windows") {
+        cfg.define("HAVE_UNISTD_H", None);
+        cfg.define("HAVE_SYS_MMAN_H", None);
     }
+
+    // We know that these are present in our vendored freetype
+    cfg.define("HAVE_FREETYPE", Some("1"));
+    cfg.define("HAVE_FT_Get_Var_Blend_Coordinates", Some("1"));
+    cfg.define("HAVE_FT_Set_Var_Blend_Coordinates", Some("1"));
+    cfg.define("HAVE_FT_Done_MM_Var", Some("1"));
+
+    if target.contains("darwin") {
+        cfg.define("HAVE_CORETEXT", None);
+        cfg.file("harfbuzz/src/hb-coretext.cc");
+    }
+
+    if target.contains("windows") {
+        cfg.define("HAVE_DIRECTWRITE", None);
+        cfg.file("harfbuzz/src/hb-directwrite.cc");
+        cfg.define("HAVE_UNISCRIBE", None);
+        cfg.file("harfbuzz/src/hb-uniscribe.cc");
+    }
+
+    // Import the include dirs exported from deps/freetype/build.rs
+    for inc in std::env::var("DEP_FREETYPE_INCLUDE").unwrap().split(";") {
+        cfg.include(inc);
+    }
+
+    cfg.compile("harfbuzz");
 }
 
 fn git_submodule_update() {
