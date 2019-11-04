e288ecce7c32d4701835322c803585f8b483cb43
Wez Furlong <wez@wezfurlong.org>
2019-11-04 08:56:11 -0800

    windows: avoid panic when clicking on links
    The `open` crate causes us to recurse into our wndproc and re-borrow,
    so avoid that and schedule a separate call later.

--- src/frontend/gui/termwindow.rs
+++ src/frontend/gui/termwindow.rs
@@ -55,10 +55,17 @@ impl<'a> term::TerminalHost for Host<'a> {
     }
 
     fn click_link(&mut self, link: &Arc<term::cell::Hyperlink>) {
-        log::error!("clicking {}", link.uri());
-        if let Err(err) = open::that(link.uri()) {
-            log::error!("failed to open {}: {:?}", link.uri(), err);
-        }
+        // Ensure that we spawn the `open` call outside of the context
+        // of our window loop; on Windows it can cause a panic due to
+        // triggering our WndProc recursively.
+        let link = link.clone();
+        promise::Future::with_executor(executor(), move || {
+            log::error!("clicking {}", link.uri());
+            if let Err(err) = open::that(link.uri()) {
+                log::error!("failed to open {}: {:?}", link.uri(), err);
+            }
+            Ok(())
+        });
     }
 }
 
