2960b9186ce5888e0cc6bac6cf6a1f0e35a56d98
Wez Furlong <wez@wezfurlong.org>
2019-11-24 07:35:12 -0800

    config.rs -> config/mod.rs + config/keys.rs

---
+++ src/config/keys.rs
@@ -0,0 +1,234 @@
+use crate::keyassignment::{KeyAssignment, SpawnTabDomain};
+use failure::{format_err, Error};
+use serde::{Deserialize, Deserializer};
+use serde_derive::*;
+use termwiz::input::{KeyCode, Modifiers};
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Key {
+    #[serde(deserialize_with = "de_keycode")]
+    pub key: KeyCode,
+    #[serde(deserialize_with = "de_modifiers")]
+    pub mods: Modifiers,
+    pub action: KeyAction,
+    pub arg: Option<String>,
+}
+
+impl std::convert::TryInto<KeyAssignment> for &Key {
+    type Error = Error;
+    fn try_into(self) -> Result<KeyAssignment, Error> {
+        Ok(match self.action {
+            KeyAction::SpawnTab => KeyAssignment::SpawnTab(SpawnTabDomain::DefaultDomain),
+            KeyAction::SpawnTabInCurrentTabDomain => {
+                KeyAssignment::SpawnTab(SpawnTabDomain::CurrentTabDomain)
+            }
+            KeyAction::SpawnTabInDomain => {
+                let arg = self
+                    .arg
+                    .as_ref()
+                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?;
+
+                if let Ok(id) = arg.parse() {
+                    KeyAssignment::SpawnTab(SpawnTabDomain::Domain(id))
+                } else {
+                    KeyAssignment::SpawnTab(SpawnTabDomain::DomainName(arg.to_string()))
+                }
+            }
+            KeyAction::SpawnWindow => KeyAssignment::SpawnWindow,
+            KeyAction::ToggleFullScreen => KeyAssignment::ToggleFullScreen,
+            KeyAction::Copy => KeyAssignment::Copy,
+            KeyAction::Paste => KeyAssignment::Paste,
+            KeyAction::Hide => KeyAssignment::Hide,
+            KeyAction::Show => KeyAssignment::Show,
+            KeyAction::IncreaseFontSize => KeyAssignment::IncreaseFontSize,
+            KeyAction::DecreaseFontSize => KeyAssignment::DecreaseFontSize,
+            KeyAction::ResetFontSize => KeyAssignment::ResetFontSize,
+            KeyAction::Nop => KeyAssignment::Nop,
+            KeyAction::CloseCurrentTab => KeyAssignment::CloseCurrentTab,
+            KeyAction::ActivateTab => KeyAssignment::ActivateTab(
+                self.arg
+                    .as_ref()
+                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?
+                    .parse()?,
+            ),
+            KeyAction::ActivateTabRelative => KeyAssignment::ActivateTabRelative(
+                self.arg
+                    .as_ref()
+                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?
+                    .parse()?,
+            ),
+            KeyAction::SendString => KeyAssignment::SendString(
+                self.arg
+                    .as_ref()
+                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?
+                    .to_owned(),
+            ),
+        })
+    }
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub enum KeyAction {
+    SpawnTab,
+    SpawnTabInCurrentTabDomain,
+    SpawnTabInDomain,
+    SpawnWindow,
+    ToggleFullScreen,
+    Copy,
+    Paste,
+    ActivateTabRelative,
+    IncreaseFontSize,
+    DecreaseFontSize,
+    ResetFontSize,
+    ActivateTab,
+    SendString,
+    Nop,
+    Hide,
+    Show,
+    CloseCurrentTab,
+}
+
+fn de_keycode<'de, D>(deserializer: D) -> Result<KeyCode, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+
+    macro_rules! m {
+        ($($val:ident),* $(,)?) => {
+            $(
+            if s == stringify!($val) {
+                return Ok(KeyCode::$val);
+            }
+            )*
+        }
+    }
+
+    m!(
+        Hyper,
+        Super,
+        Meta,
+        Cancel,
+        Backspace,
+        Tab,
+        Clear,
+        Enter,
+        Shift,
+        Escape,
+        LeftShift,
+        RightShift,
+        Control,
+        LeftControl,
+        RightControl,
+        Alt,
+        LeftAlt,
+        RightAlt,
+        Menu,
+        LeftMenu,
+        RightMenu,
+        Pause,
+        CapsLock,
+        PageUp,
+        PageDown,
+        End,
+        Home,
+        LeftArrow,
+        RightArrow,
+        UpArrow,
+        DownArrow,
+        Select,
+        Print,
+        Execute,
+        PrintScreen,
+        Insert,
+        Delete,
+        Help,
+        LeftWindows,
+        RightWindows,
+        Applications,
+        Sleep,
+        Numpad0,
+        Numpad1,
+        Numpad2,
+        Numpad3,
+        Numpad4,
+        Numpad5,
+        Numpad6,
+        Numpad7,
+        Numpad8,
+        Numpad9,
+        Multiply,
+        Add,
+        Separator,
+        Subtract,
+        Decimal,
+        Divide,
+        NumLock,
+        ScrollLock,
+        BrowserBack,
+        BrowserForward,
+        BrowserRefresh,
+        BrowserStop,
+        BrowserSearch,
+        BrowserFavorites,
+        BrowserHome,
+        VolumeMute,
+        VolumeDown,
+        VolumeUp,
+        MediaNextTrack,
+        MediaPrevTrack,
+        MediaStop,
+        MediaPlayPause,
+        ApplicationLeftArrow,
+        ApplicationRightArrow,
+        ApplicationUpArrow,
+        ApplicationDownArrow,
+    );
+
+    if s.len() > 1 && s.starts_with('F') {
+        let num: u8 = s[1..].parse().map_err(|_| {
+            serde::de::Error::custom(format!(
+                "expected F<NUMBER> function key string, got: {}",
+                s
+            ))
+        })?;
+        return Ok(KeyCode::Function(num));
+    }
+
+    let chars: Vec<char> = s.chars().collect();
+    if chars.len() == 1 {
+        Ok(KeyCode::Char(chars[0]))
+    } else {
+        Err(serde::de::Error::custom(format!(
+            "invalid KeyCode string {}",
+            s
+        )))
+    }
+}
+
+fn de_modifiers<'de, D>(deserializer: D) -> Result<Modifiers, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let mut mods = Modifiers::NONE;
+    for ele in s.split('|') {
+        if ele == "SHIFT" {
+            mods |= Modifiers::SHIFT;
+        } else if ele == "ALT" || ele == "OPT" || ele == "META" {
+            mods |= Modifiers::ALT;
+        } else if ele == "CTRL" {
+            mods |= Modifiers::CTRL;
+        } else if ele == "SUPER" || ele == "CMD" || ele == "WIN" {
+            mods |= Modifiers::SUPER;
+        } else if ele == "NONE" || ele == "" {
+            mods |= Modifiers::NONE;
+        } else {
+            return Err(serde::de::Error::custom(format!(
+                "invalid modifier name {} in {}",
+                ele, s
+            )));
+        }
+    }
+    Ok(mods)
+}
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -3,11 +3,10 @@
 use crate::create_user_owned_dirs;
 use crate::font::FontSystemSelection;
 use crate::frontend::FrontEndSelection;
-use crate::keyassignment::{KeyAssignment, SpawnTabDomain};
+use crate::keyassignment::KeyAssignment;
 use failure::{bail, err_msg, format_err, Error, Fallible};
 use lazy_static::lazy_static;
 use portable_pty::{CommandBuilder, PtySystemSelection};
-use serde::{Deserialize, Deserializer};
 use serde_derive::*;
 use std;
 use std::collections::HashMap;
@@ -23,6 +22,9 @@ use termwiz::color::{ColorSpec, RgbColor};
 use termwiz::hyperlink;
 use termwiz::input::{KeyCode, Modifiers};
 use toml;
+
+mod keys;
+pub use keys::*;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Config {
@@ -168,235 +170,6 @@ fn default_true() -> bool {
 
 fn default_swap_backspace_and_delete() -> bool {
     cfg!(target_os = "macos")
-}
-
-#[derive(Debug, Deserialize, Clone)]
-pub struct Key {
-    #[serde(deserialize_with = "de_keycode")]
-    pub key: KeyCode,
-    #[serde(deserialize_with = "de_modifiers")]
-    pub mods: Modifiers,
-    pub action: KeyAction,
-    pub arg: Option<String>,
-}
-
-impl std::convert::TryInto<KeyAssignment> for &Key {
-    type Error = Error;
-    fn try_into(self) -> Result<KeyAssignment, Error> {
-        Ok(match self.action {
-            KeyAction::SpawnTab => KeyAssignment::SpawnTab(SpawnTabDomain::DefaultDomain),
-            KeyAction::SpawnTabInCurrentTabDomain => {
-                KeyAssignment::SpawnTab(SpawnTabDomain::CurrentTabDomain)
-            }
-            KeyAction::SpawnTabInDomain => {
-                let arg = self
-                    .arg
-                    .as_ref()
-                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?;
-
-                if let Ok(id) = arg.parse() {
-                    KeyAssignment::SpawnTab(SpawnTabDomain::Domain(id))
-                } else {
-                    KeyAssignment::SpawnTab(SpawnTabDomain::DomainName(arg.to_string()))
-                }
-            }
-            KeyAction::SpawnWindow => KeyAssignment::SpawnWindow,
-            KeyAction::ToggleFullScreen => KeyAssignment::ToggleFullScreen,
-            KeyAction::Copy => KeyAssignment::Copy,
-            KeyAction::Paste => KeyAssignment::Paste,
-            KeyAction::Hide => KeyAssignment::Hide,
-            KeyAction::Show => KeyAssignment::Show,
-            KeyAction::IncreaseFontSize => KeyAssignment::IncreaseFontSize,
-            KeyAction::DecreaseFontSize => KeyAssignment::DecreaseFontSize,
-            KeyAction::ResetFontSize => KeyAssignment::ResetFontSize,
-            KeyAction::Nop => KeyAssignment::Nop,
-            KeyAction::CloseCurrentTab => KeyAssignment::CloseCurrentTab,
-            KeyAction::ActivateTab => KeyAssignment::ActivateTab(
-                self.arg
-                    .as_ref()
-                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?
-                    .parse()?,
-            ),
-            KeyAction::ActivateTabRelative => KeyAssignment::ActivateTabRelative(
-                self.arg
-                    .as_ref()
-                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?
-                    .parse()?,
-            ),
-            KeyAction::SendString => KeyAssignment::SendString(
-                self.arg
-                    .as_ref()
-                    .ok_or_else(|| format_err!("missing arg for {:?}", self))?
-                    .to_owned(),
-            ),
-        })
-    }
-}
-
-#[derive(Debug, Deserialize, Clone)]
-pub enum KeyAction {
-    SpawnTab,
-    SpawnTabInCurrentTabDomain,
-    SpawnTabInDomain,
-    SpawnWindow,
-    ToggleFullScreen,
-    Copy,
-    Paste,
-    ActivateTabRelative,
-    IncreaseFontSize,
-    DecreaseFontSize,
-    ResetFontSize,
-    ActivateTab,
-    SendString,
-    Nop,
-    Hide,
-    Show,
-    CloseCurrentTab,
-}
-
-fn de_keycode<'de, D>(deserializer: D) -> Result<KeyCode, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-
-    macro_rules! m {
-        ($($val:ident),* $(,)?) => {
-            $(
-            if s == stringify!($val) {
-                return Ok(KeyCode::$val);
-            }
-            )*
-        }
-    }
-
-    m!(
-        Hyper,
-        Super,
-        Meta,
-        Cancel,
-        Backspace,
-        Tab,
-        Clear,
-        Enter,
-        Shift,
-        Escape,
-        LeftShift,
-        RightShift,
-        Control,
-        LeftControl,
-        RightControl,
-        Alt,
-        LeftAlt,
-        RightAlt,
-        Menu,
-        LeftMenu,
-        RightMenu,
-        Pause,
-        CapsLock,
-        PageUp,
-        PageDown,
-        End,
-        Home,
-        LeftArrow,
-        RightArrow,
-        UpArrow,
-        DownArrow,
-        Select,
-        Print,
-        Execute,
-        PrintScreen,
-        Insert,
-        Delete,
-        Help,
-        LeftWindows,
-        RightWindows,
-        Applications,
-        Sleep,
-        Numpad0,
-        Numpad1,
-        Numpad2,
-        Numpad3,
-        Numpad4,
-        Numpad5,
-        Numpad6,
-        Numpad7,
-        Numpad8,
-        Numpad9,
-        Multiply,
-        Add,
-        Separator,
-        Subtract,
-        Decimal,
-        Divide,
-        NumLock,
-        ScrollLock,
-        BrowserBack,
-        BrowserForward,
-        BrowserRefresh,
-        BrowserStop,
-        BrowserSearch,
-        BrowserFavorites,
-        BrowserHome,
-        VolumeMute,
-        VolumeDown,
-        VolumeUp,
-        MediaNextTrack,
-        MediaPrevTrack,
-        MediaStop,
-        MediaPlayPause,
-        ApplicationLeftArrow,
-        ApplicationRightArrow,
-        ApplicationUpArrow,
-        ApplicationDownArrow,
-    );
-
-    if s.len() > 1 && s.starts_with('F') {
-        let num: u8 = s[1..].parse().map_err(|_| {
-            serde::de::Error::custom(format!(
-                "expected F<NUMBER> function key string, got: {}",
-                s
-            ))
-        })?;
-        return Ok(KeyCode::Function(num));
-    }
-
-    let chars: Vec<char> = s.chars().collect();
-    if chars.len() == 1 {
-        Ok(KeyCode::Char(chars[0]))
-    } else {
-        Err(serde::de::Error::custom(format!(
-            "invalid KeyCode string {}",
-            s
-        )))
-    }
-}
-
-fn de_modifiers<'de, D>(deserializer: D) -> Result<Modifiers, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?;
-    let mut mods = Modifiers::NONE;
-    for ele in s.split('|') {
-        if ele == "SHIFT" {
-            mods |= Modifiers::SHIFT;
-        } else if ele == "ALT" || ele == "OPT" || ele == "META" {
-            mods |= Modifiers::ALT;
-        } else if ele == "CTRL" {
-            mods |= Modifiers::CTRL;
-        } else if ele == "SUPER" || ele == "CMD" || ele == "WIN" {
-            mods |= Modifiers::SUPER;
-        } else if ele == "NONE" || ele == "" {
-            mods |= Modifiers::NONE;
-        } else {
-            return Err(serde::de::Error::custom(format!(
-                "invalid modifier name {} in {}",
-                ele, s
-            )));
-        }
-    }
-    Ok(mods)
 }
 
 fn default_hyperlink_rules() -> Vec<hyperlink::Rule> {
