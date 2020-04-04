466961f9c3f01d5ef3f2290380ec4534693541db
Wez Furlong <wez@wezfurlong.org>
2020-04-04 13:35:13 -0700

    tabout: allow tabulating Change slices for colorized output

--- tabout/src/lib.rs
+++ tabout/src/lib.rs
@@ -2,7 +2,8 @@
 //! so that it is presented reasonably nicely for humans to read,
 //! without requiring that each column be hard coded to particular
 //! widths in the code beforehand.
-use termwiz::cell::unicode_column_width;
+use termwiz::cell::{unicode_column_width, CellAttributes};
+use termwiz::surface::Change;
 
 /// Describes the alignment of a column
 #[derive(Debug, Clone, Copy)]
@@ -63,7 +64,10 @@ pub fn tabulate_output<S: std::string::ToString, W: std::io::Write>(
     rows: &[Vec<S>],
     output: &mut W,
 ) -> Result<(), std::io::Error> {
-    let mut col_widths: Vec<usize> = columns.iter().map(|c| c.name.len()).collect();
+    let mut col_widths: Vec<usize> = columns
+        .iter()
+        .map(|c| unicode_column_width(&c.name))
+        .collect();
 
     let mut display_rows: Vec<Vec<String>> = vec![];
     for src_row in rows {
@@ -106,6 +110,110 @@ pub fn tabulate_output<S: std::string::ToString, W: std::io::Write>(
     }
 
     Ok(())
+}
+
+pub fn unicode_column_width_of_change_slice(s: &[Change]) -> usize {
+    s.iter()
+        .map(|c| {
+            if c.is_text() {
+                unicode_column_width(c.text())
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn emit_padding_for_terminal(len: usize, spacer: &CellAttributes, output: &mut Vec<Change>) {
+    if len == 0 {
+        return;
+    }
+    output.push(Change::AllAttributes(spacer.clone()));
+    let mut s = String::new();
+    for _ in 0..len {
+        s.push(' ');
+    }
+    output.push(s.into());
+}
+
+fn emit_column_for_terminal(
+    s: &[Change],
+    max_width: usize,
+    alignment: Alignment,
+    output: &mut Vec<Change>,
+    spacer: &CellAttributes,
+) {
+    let text_width = unicode_column_width_of_change_slice(s);
+    let (left_pad, right_pad) = match alignment {
+        Alignment::Left => (0, max_width - text_width),
+        Alignment::Center => {
+            let left_pad = (max_width - text_width) / 2;
+            // for odd-length columns, take care to use the remaining
+            // length rather than just assuming that the right_pad
+            // will have the same value as the left_pad
+            let right_pad = max_width - (text_width + left_pad);
+            (left_pad, right_pad)
+        }
+        Alignment::Right => (max_width - text_width, 0),
+    };
+
+    emit_padding_for_terminal(left_pad, spacer, output);
+    output.extend_from_slice(s);
+    emit_padding_for_terminal(right_pad, spacer, output);
+}
+
+pub fn tabulate_for_terminal(
+    columns: &[Column],
+    rows: &[Vec<Vec<Change>>],
+    spacer: CellAttributes,
+    result: &mut Vec<Change>,
+) {
+    let mut col_widths: Vec<usize> = columns
+        .iter()
+        .map(|c| unicode_column_width(&c.name))
+        .collect();
+
+    for row in rows {
+        for (idx, col) in row.iter().enumerate() {
+            let col_width = unicode_column_width_of_change_slice(col);
+            if let Some(width) = col_widths.get_mut(idx) {
+                *width = (*width).max(col_width);
+            } else {
+                col_widths.push(col_width);
+            }
+        }
+    }
+
+    for (idx, col) in columns.iter().enumerate() {
+        if idx > 0 {
+            emit_padding_for_terminal(1, &spacer, result);
+        }
+        emit_column_for_terminal(
+            &[col.name.clone().into()],
+            col_widths[idx],
+            col.alignment,
+            result,
+            &spacer,
+        );
+    }
+    result.push("\r\n".into());
+
+    for row in rows {
+        for (idx, col) in row.iter().enumerate() {
+            let max_width = col_widths.get(idx).cloned().unwrap_or_else(|| col.len());
+            let alignment = columns
+                .get(idx)
+                .map(|c| c.alignment)
+                .unwrap_or(Alignment::Left);
+
+            if idx > 0 {
+                emit_padding_for_terminal(1, &spacer, result);
+            }
+
+            emit_column_for_terminal(col, max_width, alignment, result, &spacer);
+        }
+        result.push("\r\n".into());
+    }
 }
 
 /// A convenience around `tabulate_output` that returns a String holding
