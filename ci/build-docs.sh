ae5cfec1f4da322fb9fd9fd7b508a0b3c876df69
Wez Furlong <wez@wezfurlong.org>
2020-04-06 17:39:57 -0700

    docs: enable syntax highlighting for lua
    This is a bit gross, but it works!

--- ci/build-docs.sh
+++ ci/build-docs.sh
@@ -4,3 +4,10 @@
 python3 ci/subst-release-info.py
 mdbook build docs
 
+# mdBook can append js includes but it is too late to register syntax
+# highlighting extensions, so we apply brute force here
+
+mv gh_pages/book.js gh_pages/book.2
+cat docs/lua.js gh_pages/book.2 > gh_pages/book.js
+rm gh_pages/book.2
+
