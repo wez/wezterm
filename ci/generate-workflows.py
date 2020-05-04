307714437b70015d82e1509645a745f94ea253a4
Wez Furlong <wez@wezfurlong.org>
2020-05-04 08:21:30 -0700

    CI: add centos 8 builds

--- ci/generate-workflows.py
+++ ci/generate-workflows.py
@@ -419,6 +419,7 @@ TARGETS = [
     Target(container="fedora:31"),
     Target(container="fedora:32"),
     Target(container="centos:7", bootstrap_git=True),
+    Target(container="centos:8"),
     Target(name="windows", os="vs2017-win2016", rust_target="x86_64-pc-windows-msvc"),
 ]
 
