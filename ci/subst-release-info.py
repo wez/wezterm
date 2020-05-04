c207f2b5457fa4cf38a9671dc964bc36c471f858
Wez Furlong <wez@wezfurlong.org>
2020-05-03 23:10:42 -0700

    prep docs for ubuntu 20 download

--- ci/subst-release-info.py
+++ ci/subst-release-info.py
@@ -16,6 +16,7 @@ CATEGORIZE = {
     r"^wezterm-\d+-\d+-[a-f0-9]+.deb$": "ubuntu16_deb",
     r"Ubuntu18.04.deb$": "ubuntu18_deb",
     r"Ubuntu19.10.deb$": "ubuntu19_deb",
+    r"Ubuntu20.04.deb$": "ubuntu20_deb",
     r"Ubuntu16.04.tar.xz$": "linux_raw_bin",
     r"^wezterm-\d+-\d+-[a-f0-9]+.tar.xz$": "linux_raw_bin",
     r"src.tar.gz$": "src",
