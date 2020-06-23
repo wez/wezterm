wez/wezterm

commit 4e6357b21829d8549ebbb0750d8f7faad9fffc70

term: enable DECSET 1004 Focus Reporting
This is enabled only for local terminals for now; the multiplexer
protocol will need to be extended to carry this signal.

src/frontend/gui/termwindow.rs
@@ -320,6 +320,10 @@ impl WindowCallbacks for TermWindow {
 
         // force cursor to be repainted
         self.window.as_ref().unwrap().invalidate();
+
+        if let Some(tab) = self.get_active_tab_or_overlay() {
+            tab.focus_changed(focused);
+        }
     }
 
     fn mouse_event(&mut self, event: &MouseEvent, context: &dyn WindowOps) {
@@ -1216,10 +1220,19 @@ impl TermWindow {
             tab_idx as usize
         };
 
+        if let Some(tab) = self.get_active_tab_or_overlay() {
+            tab.focus_changed(false);
+        }
+
         if tab_idx < max {
             window.set_active(tab_idx);
 
             drop(window);
+
+            if let Some(tab) = self.get_active_tab_or_overlay() {
+                tab.focus_changed(false);
+            }
+
             self.update_title();
             self.update_scrollbar();
         }

src/localtab.rs
@@ -94,6 +94,10 @@ impl Tab for LocalTab {
         self.terminal.borrow_mut().erase_scrollback();
     }
 
+    fn focus_changed(&self, focused: bool) {
+        self.terminal.borrow_mut().focus_changed(focused);
+    }
+
     fn is_mouse_grabbed(&self) -> bool {
         self.terminal.borrow().is_mouse_grabbed()
     }

src/mux/tab.rs
@@ -102,6 +102,9 @@ pub trait Tab: Downcast {
 
     fn erase_scrollback(&self) {}
 
+    /// Called to advise on whether this tab has focus
+    fn focus_changed(&self, _focused: bool) {}
+
     /// Performs a search.
     /// If the result is empty then there are no matches.
     /// Otherwise, the result shall contain all possible matches.

term/src/terminalstate.rs
@@ -261,6 +261,7 @@ pub struct TerminalState {
 
     /// Movement events enabled
     any_event_mouse: bool,
+    focus_tracking: bool,
     /// SGR style mouse tracking and reporting is enabled
     sgr_mouse: bool,
     mouse_tracking: bool,
@@ -360,6 +361,7 @@ impl TerminalState {
             color_map,
             application_keypad: false,
             bracketed_paste: false,
+            focus_tracking: false,
             sgr_mouse: false,
             any_event_mouse: false,
             button_event_mouse: false,
@@ -657,6 +659,13 @@ impl TerminalState {
     /// GUI application to decide about fragmenting a large paste.
     pub fn bracketed_paste_enabled(&self) -> bool {
         self.bracketed_paste
+    }
+
+    /// Advise the terminal about a change in its focus state
+    pub fn focus_changed(&mut self, focused: bool) {
+        if self.focus_tracking {
+            write!(self.writer, "{}{}", CSI, if focused { "I" } else { "O" }).ok();
+        }
     }
 
     /// Send text to the terminal that is the result of pasting.
@@ -1666,9 +1675,11 @@ impl TerminalState {
                 self.any_event_mouse = false;
             }
 
-            Mode::SetDecPrivateMode(DecPrivateMode::Code(DecPrivateModeCode::FocusTracking))
-            | Mode::ResetDecPrivateMode(DecPrivateMode::Code(DecPrivateModeCode::FocusTracking)) => {
-                // FocusTracking is not supported
+            Mode::SetDecPrivateMode(DecPrivateMode::Code(DecPrivateModeCode::FocusTracking)) => {
+                self.focus_tracking = true;
+            }
+            Mode::ResetDecPrivateMode(DecPrivateMode::Code(DecPrivateModeCode::FocusTracking)) => {
+                self.focus_tracking = false;
             }
 
             Mode::SetDecPrivateMode(DecPrivateMode::Code(DecPrivateModeCode::SGRMouse)) => {
@@ -2739,6 +2750,7 @@ impl<'a> Performer<'a> {
                 self.dec_ansi_mode = false;
                 self.application_keypad = false;
                 self.bracketed_paste = false;
+                self.focus_tracking = false;
                 self.sgr_mouse = false;
                 self.any_event_mouse = false;
                 self.button_event_mouse = false;

commit 5b654710cbe5e4fad6051419ed7da0e3cbd90d98

wezterm: fix `already mutably borrowed: BorrowError` when switching tabs
Not sure why this didn't bubble up earlier, but the issue was introduced
by https://github.com/wez/wezterm/commit/4e6357b21829d8549ebbb0750d8f7faad9fffc70

And it was not setting focus=true on the new tab either :-/

refs: https://github.com/wez/wezterm/issues/232

src/frontend/gui/termwindow.rs
@@ -1207,6 +1207,10 @@ impl TermWindow {
     }
 
     fn activate_tab(&mut self, tab_idx: isize) -> anyhow::Result<()> {
+        if let Some(tab) = self.get_active_tab_or_overlay() {
+            tab.focus_changed(false);
+        }
+
         let mux = Mux::get().unwrap();
         let mut window = mux
             .get_window_mut(self.mux_window_id)
@@ -1220,17 +1224,13 @@ impl TermWindow {
             tab_idx as usize
         };
 
-        if let Some(tab) = self.get_active_tab_or_overlay() {
-            tab.focus_changed(false);
-        }
-
         if tab_idx < max {
             window.set_active(tab_idx);
 
             drop(window);
 
             if let Some(tab) = self.get_active_tab_or_overlay() {
-                tab.focus_changed(false);
+                tab.focus_changed(true);
             }
 
             self.update_title();