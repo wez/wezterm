wez/wezterm

commit 80214319aec9d7b72217db3d7a6f8e8575081eac

adjust log levels
Revise logging so that we use info level for things that we want
to always log, and adjust the logger config to always log info
level messages.

That means shifting some warning level logs down lower to debug level so
that they aren't noisy.

closes: https://github.com/wez/wezterm/issues/388

wezterm-gui/src/main.rs
@@ -416,7 +416,9 @@ fn run() -> anyhow::Result<()> {
 
     env_bootstrap::bootstrap();
 
-    pretty_env_logger::init_timed();
+    pretty_env_logger::formatted_timed_builder()
+        .filter(None, log::LevelFilter::Info)
+        .init();
     stats::Stats::init()?;
     let _saver = umask::UmaskSaver::new();
 
@@ -442,7 +444,7 @@ fn run() -> anyhow::Result<()> {
         .unwrap_or_else(|| SubCommand::Start(StartCommand::default()))
     {
         SubCommand::Start(start) => {
-            log::info!("Using configuration: {:#?}\nopts: {:#?}", config, opts);
+            log::trace!("Using configuration: {:#?}\nopts: {:#?}", config, opts);
             run_terminal_gui(config, start)
         }
         SubCommand::Ssh(ssh) => run_ssh(config, ssh),

wezterm-mux-server/src/main.rs
@@ -45,7 +45,9 @@ struct Opt {
 }
 
 fn main() {
-    pretty_env_logger::init_timed();
+    pretty_env_logger::formatted_timed_builder()
+        .filter(None, log::LevelFilter::Info)
+        .init();
     if let Err(err) = run() {
         log::error!("{:#}", err);
         std::process::exit(1);

wezterm/src/main.rs
@@ -223,7 +223,9 @@ fn main() {
 fn run() -> anyhow::Result<()> {
     env_bootstrap::bootstrap();
 
-    pretty_env_logger::init_timed();
+    pretty_env_logger::formatted_timed_builder()
+        .filter(None, log::LevelFilter::Info)
+        .init();
     let _saver = umask::UmaskSaver::new();
 
     let opts = Opt::from_args();

mux/src/lib.rs
@@ -141,7 +141,7 @@ fn read_from_pane_pty(pane_id: PaneId, mut reader: Box<dyn std::io::Read>) {
     while !dead.load(Ordering::Relaxed) {
         match reader.read(&mut buf) {
             Ok(size) if size == 0 => {
-                error!("read_pty EOF: pane_id {}", pane_id);
+                log::trace!("read_pty EOF: pane_id {}", pane_id);
                 break;
             }
             Err(err) => {
@@ -401,7 +401,7 @@ impl Mux {
         }
 
         for tab_id in dead_tab_ids {
-            log::error!("tab {} is dead", tab_id);
+            log::trace!("tab {} is dead", tab_id);
             self.remove_tab_internal(tab_id);
         }
 

mux/src/domain.rs
@@ -14,7 +14,6 @@ use anyhow::{bail, Error};
 use async_trait::async_trait;
 use config::configuration;
 use downcast_rs::{impl_downcast, Downcast};
-use log::info;
 use portable_pty::{native_pty_system, CommandBuilder, PtySize, PtySystem};
 use std::rc::Rc;
 
@@ -135,7 +134,7 @@ impl Domain for LocalDomain {
         cmd.env("WEZTERM_PANE", pane_id.to_string());
 
         let child = pair.slave.spawn_command(cmd)?;
-        info!("spawned: {:?}", child);
+        log::trace!("spawned: {:?}", child);
 
         let writer = pair.master.try_clone_writer()?;
 
@@ -217,7 +216,7 @@ impl Domain for LocalDomain {
         let pane_id = alloc_pane_id();
         cmd.env("WEZTERM_PANE", pane_id.to_string());
         let child = pair.slave.spawn_command(cmd)?;
-        info!("spawned: {:?}", child);
+        log::trace!("spawned: {:?}", child);
 
         let writer = pair.master.try_clone_writer()?;
 

mux/src/localpane.rs
@@ -79,7 +79,7 @@ impl Pane for LocalPane {
         if let Ok(None) = self.process.borrow_mut().try_wait() {
             false
         } else {
-            log::error!("Pane id {} is_dead", self.pane_id);
+            log::trace!("Pane id {} is_dead", self.pane_id);
             true
         }
     }

mux/src/ssh.rs
@@ -177,7 +177,7 @@ pub fn ssh_connect_with_ui(
 
             if !sess.authenticated() && methods.contains("publickey") {
                 if let Err(err) = sess.userauth_agent(&username) {
-                    log::info!("while attempting agent auth: {}", err);
+                    log::warn!("while attempting agent auth: {}", err);
                 } else if sess.authenticated() {
                     ui.output_str("publickey auth successful!\n");
                 }
@@ -251,7 +251,7 @@ impl Domain for RemoteSshDomain {
         let pane_id = alloc_pane_id();
         cmd.env("WEZTERM_PANE", pane_id.to_string());
         let child = pair.slave.spawn_command(cmd)?;
-        log::info!("spawned: {:?}", child);
+        log::trace!("spawned: {:?}", child);
 
         let writer = pair.master.try_clone_writer()?;
 

term/src/terminalstate.rs
@@ -1497,7 +1497,7 @@ impl TerminalState {
 
     fn perform_device(&mut self, dev: Device) {
         match dev {
-            Device::DeviceAttributes(a) => error!("unhandled: {:?}", a),
+            Device::DeviceAttributes(a) => log::warn!("unhandled: {:?}", a),
             Device::SoftReset => {
                 // TODO: see https://vt100.net/docs/vt510-rm/DECSTR.html
                 self.pen = CellAttributes::default();
@@ -3027,6 +3027,10 @@ impl<'a> Performer<'a> {
             ) => {
                 self.pen.set_semantic_type(SemanticType::Output);
             }
+
+            OperatingSystemCommand::FinalTermSemanticPrompt(
+                FinalTermSemanticPrompt::CommandStatus { .. },
+            ) => {}
 
             OperatingSystemCommand::FinalTermSemanticPrompt(ft) => {
                 error!("unhandled: {:?}", ft);

commit c66e8f5c5e2fac020ebcc70d19a4dedf1d06483a

use $WEZTERM_LOG to configure log levels ad-hoc
80214319aec9d7b72217db3d7a6f8e8575081eac broke the use of RUST_LOG to
turn up trace logging.

This commit refactors logger initialization into the env-bootstrap crate
so that it is centralized, and adopts the use of `WEZTERM_LOG` to
override the default logging filters, rather than `RUST_LOG`.

env-bootstrap/Cargo.toml
@@ -9,6 +9,8 @@ edition = "2018"
 [dependencies]
 config = { path = "../config" }
 dirs-next = "2.0"
+log = "0.4"
+pretty_env_logger = "0.4"
 
 [target.'cfg(target_os = "macos")'.dependencies]
 cocoa = "0.20"

env-bootstrap/src/lib.rs
@@ -9,6 +9,16 @@ pub fn set_wezterm_executable() {
     }
 }
 
+pub fn setup_logger() {
+    let mut builder = pretty_env_logger::formatted_timed_builder();
+    if let Ok(s) = std::env::var("WEZTERM_LOG") {
+        builder.parse_filters(&s);
+    } else {
+        builder.filter(None, log::LevelFilter::Info);
+    }
+    builder.init();
+}
+
 pub fn fixup_appimage() {
     if let Some(appimage) = std::env::var_os("APPIMAGE") {
         let appimage = std::path::PathBuf::from(appimage);
@@ -118,4 +128,6 @@ pub fn bootstrap() {
     set_lang_from_locale();
 
     fixup_appimage();
+
+    setup_logger();
 }

wezterm-gui/Cargo.toml
@@ -38,7 +38,6 @@ open = "1.4"
 openssl = "0.10"
 palette = "0.5"
 portable-pty = { path = "../pty", features = ["serde_support", "ssh"]}
-pretty_env_logger = "0.4"
 promise = { path = "../promise" }
 pulldown-cmark = "0.8"
 rangeset = { path = "../rangeset" }

wezterm-gui/src/main.rs
@@ -416,9 +416,6 @@ fn run() -> anyhow::Result<()> {
 
     env_bootstrap::bootstrap();
 
-    pretty_env_logger::formatted_timed_builder()
-        .filter(None, log::LevelFilter::Info)
-        .init();
     stats::Stats::init()?;
     let _saver = umask::UmaskSaver::new();
 

wezterm-mux-server/Cargo.toml
@@ -20,7 +20,6 @@ log = "0.4"
 mux = { path = "../mux" }
 openssl = "0.10"
 portable-pty = { path = "../pty", features = ["serde_support"]}
-pretty_env_logger = "0.4"
 promise = { path = "../promise" }
 rangeset = { path = "../rangeset" }
 rcgen = "0.8"

wezterm-mux-server/src/main.rs
@@ -45,9 +45,6 @@ struct Opt {
 }
 
 fn main() {
-    pretty_env_logger::formatted_timed_builder()
-        .filter(None, log::LevelFilter::Info)
-        .init();
     if let Err(err) = run() {
         log::error!("{:#}", err);
         std::process::exit(1);

wezterm/Cargo.toml
@@ -19,7 +19,6 @@ hostname = "0.3"
 log = "0.4"
 mux = { path = "../mux" }
 portable-pty = { path = "../pty" }
-pretty_env_logger = "0.4"
 promise = { path = "../promise" }
 structopt = "0.3"
 tabout = { path = "../tabout" }

wezterm/src/main.rs
@@ -223,9 +223,6 @@ fn main() {
 fn run() -> anyhow::Result<()> {
     env_bootstrap::bootstrap();
 
-    pretty_env_logger::formatted_timed_builder()
-        .filter(None, log::LevelFilter::Info)
-        .init();
     let _saver = umask::UmaskSaver::new();
 
     let opts = Opt::from_args();