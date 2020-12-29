972c07a6920ab1163e0ad4779835663ef751cd97
Wez Furlong <wez@wezfurlong.org>
2020-12-29 13:15:16 -0800

    window: update examples for opengl-only-ness

--- window/examples/basic.rs
+++ window/examples/basic.rs
@@ -1,9 +1,24 @@
 use ::window::*;
+use glium::{program, uniform};
 use std::any::Any;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    color: [f32; 3],
+}
+glium::implement_vertex!(Vertex, position, color);
 
 struct MyWindow {
     allow_close: bool,
     cursor_pos: Point,
+    gl: Option<GlState>,
+}
+
+struct GlState {
+    vb: glium::VertexBuffer<Vertex>,
+    index_buffer: glium::IndexBuffer<u16>,
+    program: glium::Program,
 }
 
 impl Drop for MyWindow {
@@ -26,6 +41,96 @@ impl WindowCallbacks for MyWindow {
     fn destroy(&mut self) {
         eprintln!("destroy was called!");
         Connection::get().unwrap().terminate_message_loop();
+    }
+
+    fn created(
+        &mut self,
+        _window: &Window,
+        context: std::rc::Rc<glium::backend::Context>,
+    ) -> anyhow::Result<()> {
+        let vb = glium::VertexBuffer::new(
+            &context,
+            &[
+                Vertex {
+                    position: [-0.5, -0.5],
+                    color: [0.0, 1.0, 0.0],
+                },
+                Vertex {
+                    position: [0.0, 0.5],
+                    color: [0.0, 0.0, 1.0],
+                },
+                Vertex {
+                    position: [0.5, -0.5],
+                    color: [1.0, 0.0, 0.0],
+                },
+            ],
+        )?;
+
+        let index_buffer = glium::IndexBuffer::new(
+            &context,
+            glium::index::PrimitiveType::TrianglesList,
+            &[0u16, 1, 2],
+        )?;
+
+        let program = glium::program!(&context,
+                                      140 => {
+            vertex: "
+                #version 140
+                uniform mat4 matrix;
+                in vec2 position;
+                in vec3 color;
+                out vec3 vColor;
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 140
+                in vec3 vColor;
+                out vec4 f_color;
+                void main() {
+                    f_color = vec4(vColor, 1.0);
+                }
+            "
+        },
+        )?;
+
+        self.gl.replace(GlState {
+            vb,
+            index_buffer,
+            program,
+        });
+
+        Ok(())
+    }
+
+    fn paint_opengl(&mut self, frame: &mut glium::Frame) {
+        // Window contents are gray in opengl mode
+        use glium::Surface;
+        frame.clear_color_srgb(0.25, 0.125, 0.375, 1.0);
+
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32]
+            ]
+        };
+
+        // drawing a frame
+        let gl = self.gl.as_ref().unwrap();
+        frame
+            .draw(
+                &gl.vb,
+                &gl.index_buffer,
+                &gl.program,
+                &uniforms,
+                &Default::default(),
+            )
+            .unwrap();
     }
 
     fn paint(&mut self, context: &mut dyn PaintContext) {
@@ -91,6 +196,7 @@ fn spawn_window() -> anyhow::Result<()> {
         Box::new(MyWindow {
             allow_close: false,
             cursor_pos: Point::new(100, 200),
+            gl: None,
         }),
     )?;
 
