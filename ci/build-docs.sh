5c41afaccee67fff0c070f8e7f5635526141780b
Wez Furlong <wez@wezfurlong.org>
2021-02-01 08:12:21 -0800

    docs: remove lua syntax highlighting hack
    The updated mdbook bits already include this, and our hack resulted
    in disabling lua highlighting.

--- ci/build-docs.sh
+++ ci/build-docs.sh
@@ -5,11 +5,5 @@ python3 ci/subst-release-info.py || exit 1
 python3 ci/generate-docs.py || exit 1
 mdbook build docs
 
-# mdBook can append js includes but it is too late to register syntax
-# highlighting extensions, so we apply brute force here
-
-mv gh_pages/html/book.js gh_pages/html/book.2
-cat docs/lua.js gh_pages/html/book.2 > gh_pages/html/book.js
-rm gh_pages/html/book.2
 cp assets/icon/terminal.png gh_pages/html/favicon.png
 cp assets/icon/wezterm-icon.svg gh_pages/html/favicon.svg
