35060fb2dec0b048044c9fa1197aafb64626ca5d
Wez Furlong <wez@wezfurlong.org>
2021-02-24 09:27:13 -0800

    Revert "ci: disable mdbook-linkcheck"
    This reverts commit bef0fa29ac7e7dccc4e119d4ec9070e0767a539c and
    bb2d45d0aa930245188c17f7695663e4f3becbb3.
    
    refs: https://github.com/Michael-F-Bryan/mdbook-linkcheck/issues/53

--- ci/build-docs.sh
+++ ci/build-docs.sh
@@ -5,5 +5,5 @@ python3 ci/subst-release-info.py || exit 1
 python3 ci/generate-docs.py || exit 1
 mdbook build docs
 
-cp assets/icon/terminal.png gh_pages/favicon.png
-cp assets/icon/wezterm-icon.svg gh_pages/favicon.svg
+cp assets/icon/terminal.png gh_pages/html/favicon.png
+cp assets/icon/wezterm-icon.svg gh_pages/html/favicon.svg
