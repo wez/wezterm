wez/wezterm

commit b441be3ac92d7b2390fd416fe5c76da13679aa30

gui: vertically center cell within adjusted line height
When line_height is specified, rather than applying the offset
to just the top of the cell, apply it in equal parts to the top
and the bottom so that the cell is vertically centered.

closes: https://github.com/wez/wezterm/issues/582

wezterm-gui/src/utilsprites.rs
@@ -33,16 +33,21 @@ impl RenderMetrics {
             metrics.cell_width.get().ceil() as usize,
         );
 
+        // When line_height != 1.0, we want to adjust the baseline position
+        // such that we are horizontally centered.
+        let line_height_y_adjust = (cell_height as f64 - metrics.cell_height.get()) / 2.;
+
         let underline_height = metrics.underline_thickness.get().round().max(1.) as isize;
 
-        let descender_row =
-            (cell_height as f64 + (metrics.descender - metrics.underline_position).get()) as isize;
+        let descender_row = (cell_height as f64
+            + (metrics.descender - metrics.underline_position).get()
+            - line_height_y_adjust) as isize;
         let descender_plus_two =
             (2 * underline_height + descender_row).min(cell_height as isize - underline_height);
         let strike_row = descender_row / 2;
 
         Ok(Self {
-            descender: metrics.descender,
+            descender: metrics.descender - PixelLength::new(line_height_y_adjust),
             descender_row,
             descender_plus_two,
             strike_row,

commit 6830d171cfa7b95b2ca0addf5e817c815e19223d

fixup metrics when line_height==1.0
This was broken by b441be3ac92d7b2390fd416fe5c76da13679aa30

For whatever reason, the breakage was only visible with the Iosveka
font on Windows.  I couldn't reproduce it on my other systems, even
though the code technically applies to any system.

The breakage was: the metrics resulted in a difference of about 0.4
pixels being used for the descender with that particular font, resulting
in weird vertical alignment problems.

The offset needs to be computed against the ceil of the cell height,
which removes the fractional offset.

refs: https://github.com/wez/wezterm/issues/661
refs: https://github.com/wez/wezterm/issues/582

wezterm-gui/src/utilsprites.rs
@@ -35,7 +35,7 @@ impl RenderMetrics {
 
         // When line_height != 1.0, we want to adjust the baseline position
         // such that we are horizontally centered.
-        let line_height_y_adjust = (cell_height as f64 - metrics.cell_height.get()) / 2.;
+        let line_height_y_adjust = (cell_height as f64 - metrics.cell_height.get().ceil()) / 2.;
 
         let underline_height = metrics.underline_thickness.get().round().max(1.) as isize;
 