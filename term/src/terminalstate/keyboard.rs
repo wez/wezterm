wez/wezterm

commit 8da1f676a8cec22e0f24f360b6a001a28a0e53e7

ALT+ "function" keys incorrectly sent ESC prefixed sequences
closes: #892

termwiz/src/input.rs
@@ -379,12 +379,12 @@ impl KeyCode {
                     CSI
                 };
 
-                if mods.contains(Modifiers::SHIFT) || mods.contains(Modifiers::CTRL) {
+                if mods.contains(Modifiers::ALT)
+                    || mods.contains(Modifiers::SHIFT)
+                    || mods.contains(Modifiers::CTRL)
+                {
                     write!(buf, "{}1;{}{}", CSI, 1 + encode_modifiers(mods), c)?;
                 } else {
-                    if mods.contains(Modifiers::ALT) {
-                        buf.push(0x1b as char);
-                    }
                     write!(buf, "{}{}", csi_or_ss3, c)?;
                 }
             }
@@ -398,12 +398,12 @@ impl KeyCode {
                     _ => unreachable!(),
                 };
 
-                if mods.contains(Modifiers::SHIFT) || mods.contains(Modifiers::CTRL) {
+                if mods.contains(Modifiers::ALT)
+                    || mods.contains(Modifiers::SHIFT)
+                    || mods.contains(Modifiers::CTRL)
+                {
                     write!(buf, "\x1b[{};{}~", c, 1 + encode_modifiers(mods))?;
                 } else {
-                    if mods.contains(Modifiers::ALT) {
-                        buf.push(0x1b as char);
-                    }
                     write!(buf, "\x1b[{}~", c)?;
                 }
             }
@@ -1489,6 +1489,48 @@ mod test {
                 }),
             ],
             inputs
+        );
+    }
+
+    #[test]
+    fn encode_issue_892() {
+        let mode = KeyCodeEncodeModes {
+            enable_csi_u_key_encoding: false,
+            newline_mode: false,
+            application_cursor_keys: false,
+        };
+
+        assert_eq!(
+            KeyCode::LeftArrow.encode(Modifiers::NONE, mode).unwrap(),
+            "\x1b[D".to_string()
+        );
+        assert_eq!(
+            KeyCode::LeftArrow.encode(Modifiers::ALT, mode).unwrap(),
+            "\x1b[1;3D".to_string()
+        );
+        assert_eq!(
+            KeyCode::Home.encode(Modifiers::NONE, mode).unwrap(),
+            "\x1b[H".to_string()
+        );
+        assert_eq!(
+            KeyCode::Home.encode(Modifiers::ALT, mode).unwrap(),
+            "\x1b[1;3H".to_string()
+        );
+        assert_eq!(
+            KeyCode::End.encode(Modifiers::NONE, mode).unwrap(),
+            "\x1b[F".to_string()
+        );
+        assert_eq!(
+            KeyCode::End.encode(Modifiers::ALT, mode).unwrap(),
+            "\x1b[1;3F".to_string()
+        );
+        assert_eq!(
+            KeyCode::Tab.encode(Modifiers::ALT, mode).unwrap(),
+            "\x1b\t".to_string()
+        );
+        assert_eq!(
+            KeyCode::PageUp.encode(Modifiers::ALT, mode).unwrap(),
+            "\x1b[5;3~".to_string()
         );
     }
 }

commit 0e5d0d8117aa7a576d2b0a7da6e4473550006b6f

term: fix encoding F1-F4
This was accidentally broken by 8da1f676a8cec22e0f24f360b6a001a28a0e53e7

refs: #892

term/src/terminalstate/keyboard.rs
@@ -17,7 +17,7 @@ impl TerminalState {
             },
         )?;
 
-        // debug!("sending {:?}, {:?}", to_send, key);
+        log::trace!("sending {:?}, {:?}", to_send, key);
         self.writer.write_all(to_send.as_bytes())?;
         self.writer.flush()?;
 

termwiz/src/input.rs
@@ -411,13 +411,17 @@ impl KeyCode {
             Function(n) => {
                 if mods.is_empty() && n < 5 {
                     // F1-F4 are encoded using SS3 if there are no modifiers
-                    match n {
-                        1 => "\x1bOP",
-                        2 => "\x1bOQ",
-                        3 => "\x1bOR",
-                        4 => "\x1bOS",
-                        _ => unreachable!("wat?"),
-                    };
+                    write!(
+                        buf,
+                        "{}",
+                        match n {
+                            1 => "\x1bOP",
+                            2 => "\x1bOQ",
+                            3 => "\x1bOR",
+                            4 => "\x1bOS",
+                            _ => unreachable!("wat?"),
+                        }
+                    )?;
                 } else {
                     // Higher numbered F-keys plus modified F-keys are encoded
                     // using CSI instead of SS3.
@@ -1531,6 +1535,10 @@ mod test {
         assert_eq!(
             KeyCode::PageUp.encode(Modifiers::ALT, mode).unwrap(),
             "\x1b[5;3~".to_string()
+        );
+        assert_eq!(
+            KeyCode::Function(1).encode(Modifiers::NONE, mode).unwrap(),
+            "\x1bOP".to_string()
         );
     }
 }