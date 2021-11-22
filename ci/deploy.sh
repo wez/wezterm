98e107f307468b91c7c636fe73f30588eed711a6
Wez Furlong <wez@wezfurlong.org>
2021-11-22 12:16:55 -0700

    gah, missed a bit

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -48,6 +48,7 @@ case $OSTYPE in
 
     set +x
     if [ -n "$MACOS_CERT" ] ; then
+      echo $MACOS_CERT | base64 —decode > certificate.p12
       security create-keychain -p "$MACOS_PW" build.keychain
       security default-keychain -s build.keychain
       security unlock-keychain -p "$MACOS_PW" build.keychain
