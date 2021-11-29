30fd56c35ff5b363e36a2b7e4b2d432d282a48b7
Wez Furlong <wez@wezfurlong.org>
2021-11-28 19:10:08 -0700

    ci: actions/cache -> 2.1.7

--- ci/generate-workflows.py
+++ ci/generate-workflows.py
@@ -82,7 +82,7 @@ class ActionStep(Step):
 class CacheStep(ActionStep):
     def __init__(self, name, path, key):
         super().__init__(
-            name, action="actions/cache@v2.1.6", params={"path": path, "key": key}
+            name, action="actions/cache@v2.1.7", params={"path": path, "key": key}
         )
 
 
