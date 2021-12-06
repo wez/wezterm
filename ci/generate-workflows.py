e1260b2337b552a662825ca0df85ea00f25879e2
Wez Furlong <wez@wezfurlong.org>
2021-12-06 10:12:25 -0700

    ci: another stab at quoting for this GH key stuff
    Objectively awful, but avoids a python dep on yaml

--- ci/generate-workflows.py
+++ ci/generate-workflows.py
@@ -18,6 +18,9 @@ def yv(v, depth=0):
             for l in v.splitlines():
                 result = result + "\n" + (f"{indent}{l}" if l else "")
             return "|" + result
+        # This is hideous
+        if '"' in v:
+            return "'" + v + "'"
         return '"' + v + '"'
 
     return v
@@ -536,7 +539,7 @@ cargo build --all --release""",
                     ),
                     RunStep(
                         "Add GitHub package list",
-                        f"echo deb \\\\[arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg\\\\] https://cli.github.com/packages stable main | {sudo} tee /etc/apt/sources.list.d/github-cli.list > /dev/null",
+                        f'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | {sudo} tee /etc/apt/sources.list.d/github-cli.list > /dev/null',
                     ),
                     RunStep("Update APT", f"{sudo}apt update"),
                     RunStep("Install GH CLI", f"{sudo} apt install gh"),
