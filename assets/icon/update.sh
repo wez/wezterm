b1bc74d31ffb9f3869923e10029a5cf986d28982
Wez Furlong <wez@wezfurlong.org>
2021-12-29 18:10:54 -0700

    logo: refine appearance a bit more
    This commit switches back to wezterm-icon.svg as the source of
    the icon, but modifies it:
    
    * Removed mac style title bar + window manipulation icons
    * Increases the corner radius
    * Adjusts the text position and size
    
    This makes it somewhere between the original and one of the alternate
    icons in 98b71cbfb650ea139ffa3a001ea3e4b3fe6efcd3
    
    I chose to modify the original source as it didn't have padding
    baked into the svg file, and I didn't feel like wrestling with
    the contributed svg in inkscape to remove it.

--- assets/icon/update.sh
+++ assets/icon/update.sh
@@ -4,20 +4,16 @@
 set -x
 cd $(git rev-parse --show-toplevel)/assets/icon
 
-#src=wezterm-icon.svg
-src="wezterm-ghifarit53-2.svg"
+src=wezterm-icon.svg
 
 conv_opts="-colors 256 -background none -density 300"
 
-# Remove padding for the linux and mac versions of the icon
-crop="-crop 2580x2580+310+310"
-
 # the linux icon
-convert $conv_opts $crop -resize "!128x128" "$src" ../icon/terminal.png
+convert $conv_opts -resize "!128x128" "$src" ../icon/terminal.png
 
 for dim in 16 32 128 256 512 1024 ; do
   # convert is the imagemagick convert utility
-  convert $conv_opts -resize "!${dim}x${dim}" "$src" "icon_${dim}px.png"
+  convert $conv_opts -border '10%' -bordercolor 'rgba(0,0,0,0)' -resize "!${dim}x${dim}" "$src" "icon_${dim}px.png"
 done
 # png2icns is part of the libicns-utils on Fedora systems.
 # It glues together the various png files into a macOS .icns file
@@ -27,5 +23,5 @@ png2icns ../macos/WezTerm.app/Contents/Resources/terminal.icns icon_*px.png
 rm -f icon_*px.png
 
 # The Windows icon
-convert $conv_opts $crop -define icon:auto-resize=256,128,96,64,48,32,16 "$src" ../windows/terminal.ico
+convert $conv_opts -define icon:auto-resize=256,128,96,64,48,32,16 $src ../windows/terminal.ico
 
