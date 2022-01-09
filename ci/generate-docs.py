93c3a6381d4f3969b28aeb0d37088ba70a7ab64a
Wez Furlong <wez@wezfurlong.org>
2022-01-09 10:13:07 -0700

    docs: enable folding in navigation area
    This helps to avoid being overwhelmed by the number of pages!
    
    refs: https://github.com/rust-lang/mdBook/issues/1724

--- ci/generate-docs.py
+++ ci/generate-docs.py
@@ -137,6 +137,7 @@ TOC = [
                     Page("Key Binding", "config/keys.md"),
                     Page("Mouse Binding", "config/mouse.md"),
                     Page("Colors & Appearance", "config/appearance.md"),
+                    GenColorScheme("Color Schemes", "colorschemes"),
                 ],
             ),
             Page("Scrollback", "scrollback.md"),
@@ -152,7 +153,6 @@ TOC = [
             Page("F.A.Q.", "faq.md"),
             Page("Getting Help", "help.md"),
             Page("Contributing", "contributing.md"),
-            GenColorScheme("Color Schemes", "colorschemes"),
             Page(
                 "Lua Reference",
                 "config/lua/general.md",
