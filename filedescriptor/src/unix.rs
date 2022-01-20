1be1d012033c74b72263a0e39783381978a53a66
Nils Nieuwejaar <nils@oxide.computer>
2022-01-20 16:56:01 -0500

    fix socketpair() implementation to build on Illumos

--- filedescriptor/src/unix.rs
+++ filedescriptor/src/unix.rs
@@ -392,7 +392,12 @@ pub fn socketpair_impl() -> Result<(FileDescriptor, FileDescriptor)> {
 #[doc(hidden)]
 pub fn socketpair_impl() -> Result<(FileDescriptor, FileDescriptor)> {
     let mut fds = [-1i32; 2];
-    let res = unsafe { libc::socketpair(libc::PF_LOCAL, libc::SOCK_STREAM, 0, fds.as_mut_ptr()) };
+    #[cfg(target_os = "illumos")]
+    let domain = libc::AF_UNIX;
+    #[cfg(not(target_os = "illumos"))]
+    let domain = libc::PF_LOCAL;
+
+    let res = unsafe { libc::socketpair(domain, libc::SOCK_STREAM, 0, fds.as_mut_ptr()) };
     if res == -1 {
         Err(Error::Socketpair(std::io::Error::last_os_error()))
     } else {
