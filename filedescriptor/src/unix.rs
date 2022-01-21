4705fb96fb0307ace71b30d0a64e2397ae307f22
Nils Nieuwejaar <nils@oxidecomputer.com>
2022-01-21 18:07:56 +0000

    socketpair() uses AF_UNIX on all non-Linux systems

--- filedescriptor/src/unix.rs
+++ filedescriptor/src/unix.rs
@@ -392,12 +392,7 @@ pub fn socketpair_impl() -> Result<(FileDescriptor, FileDescriptor)> {
 #[doc(hidden)]
 pub fn socketpair_impl() -> Result<(FileDescriptor, FileDescriptor)> {
     let mut fds = [-1i32; 2];
-    #[cfg(target_os = "illumos")]
-    let domain = libc::AF_UNIX;
-    #[cfg(not(target_os = "illumos"))]
-    let domain = libc::PF_LOCAL;
-
-    let res = unsafe { libc::socketpair(domain, libc::SOCK_STREAM, 0, fds.as_mut_ptr()) };
+    let res = unsafe { libc::socketpair(libc::AF_UNIX, libc::SOCK_STREAM, 0, fds.as_mut_ptr()) };
     if res == -1 {
         Err(Error::Socketpair(std::io::Error::last_os_error()))
     } else {
