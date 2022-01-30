c80ee319e21eefc816d34b956cc26ec86bec9f62
Wez Furlong <wez@wezfurlong.org>
2022-01-30 12:13:50 -0700

    bidi: add helper for using Direction with Iterator

--- bidi/src/direction.rs
+++ bidi/src/direction.rs
@@ -29,4 +29,36 @@ impl Direction {
             Self::LeftToRight => BidiClass::LeftToRight,
         }
     }
+
+    /// Given a DoubleEndedIterator, returns an iterator that will
+    /// either walk it in its natural order if Direction==LeftToRight,
+    /// or in reverse order if Direction==RightToLeft
+    pub fn iter<I: DoubleEndedIterator<Item = T>, T>(self, iter: I) -> DirectionIter<I, T> {
+        DirectionIter::wrap(iter, self)
+    }
+}
+
+pub enum DirectionIter<I: DoubleEndedIterator<Item = T>, T> {
+    LTR(I),
+    RTL(std::iter::Rev<I>),
+}
+
+impl<I: DoubleEndedIterator<Item = T>, T> DirectionIter<I, T> {
+    pub fn wrap(iter: I, direction: Direction) -> Self {
+        match direction {
+            Direction::LeftToRight => Self::LTR(iter),
+            Direction::RightToLeft => Self::RTL(iter.rev()),
+        }
+    }
+}
+
+impl<I: DoubleEndedIterator<Item = T>, T> Iterator for DirectionIter<I, T> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Self::LTR(i) => i.next(),
+            Self::RTL(i) => i.next(),
+        }
+    }
 }
