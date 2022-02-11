36bede9b4dccbd2b1b83e03312216beef5bfdd41
Simon Schuster <git@rationality.eu>
2022-02-11 21:45:07 +0100

    Add lua wapper window:alert

--- wezterm-gui/src/scripting/guiwin.rs
+++ wezterm-gui/src/scripting/guiwin.rs
@@ -44,6 +44,10 @@ impl UserData for GuiWin {
                 Ok(())
             },
         );
+        methods.add_method("alert", |_, this, _: ()| {
+            this.window.alert();
+            Ok(())
+        },);
         methods.add_method("get_appearance", |_, _, _: ()| {
             Ok(Connection::get().unwrap().get_appearance().to_string())
         });
