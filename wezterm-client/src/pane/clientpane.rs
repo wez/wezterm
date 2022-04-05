wez/wezterm

commit f478672fd80fb8af25182b5ff8e2dcc5734e3828

mux: fix artifacts in multi-pane layouts after closing a pane
refs: https://github.com/wez/wezterm/issues/1277
refs: https://github.com/wez/wezterm/issues/783

wezterm-client/src/pane/clientpane.rs
@@ -1,4 +1,4 @@
-use crate::domain::ClientInner;
+use crate::domain::{ClientDomain, ClientInner};
 use crate::pane::mousestate::MouseState;
 use crate::pane::renderable::{hydrate_lines, RenderableInner, RenderableState};
 use anyhow::bail;
@@ -372,13 +372,29 @@ impl Pane for ClientPane {
         }
         let client = Arc::clone(&self.client);
         let remote_pane_id = self.remote_pane_id;
+        let local_domain_id = self.client.local_domain_id;
         promise::spawn::spawn(async move {
             client
                 .client
                 .kill_pane(KillPane {
                     pane_id: remote_pane_id,
                 })
-                .await
+                .await?;
+
+            // Arrange to resync the layout, to avoid artifacts
+            // <https://github.com/wez/wezterm/issues/1277>
+            let mux = Mux::get().expect("called on main thread");
+            let client_domain = mux
+                .get_domain(local_domain_id)
+                .ok_or_else(|| anyhow::anyhow!("no such domain {}", local_domain_id))?;
+            let client_domain = client_domain
+                .downcast_ref::<ClientDomain>()
+                .ok_or_else(|| {
+                    anyhow::anyhow!("domain {} is not a ClientDomain instance", local_domain_id)
+                })?;
+
+            client_domain.resync().await?;
+            anyhow::Result::<()>::Ok(())
         })
         .detach();
         // Explicitly mark ourselves as dead.

commit 9ee2d0f6b2428a8eb9704b3ff46285468363c3f3

clientpane: delay before re-sync
The fix for f478672fd80fb8af25182b5ff8e2dcc5734e3828 regressed
the one for b398eaf656b01be393ff0ac112247bb1dceb928a.

Compensate by adding a short delay.

refs: #1752

wezterm-client/src/pane/clientpane.rs
@@ -382,7 +382,11 @@ impl Pane for ClientPane {
                 .await?;
 
             // Arrange to resync the layout, to avoid artifacts
-            // <https://github.com/wez/wezterm/issues/1277>
+            // <https://github.com/wez/wezterm/issues/1277>.
+            // We need a short delay to avoid racing with the observable
+            // effects of killing the pane.
+            // <https://github.com/wez/wezterm/issues/1752#issuecomment-1088269363>
+            smol::Timer::after(std::time::Duration::from_millis(200)).await;
             let mux = Mux::get().expect("called on main thread");
             let client_domain = mux
                 .get_domain(local_domain_id)