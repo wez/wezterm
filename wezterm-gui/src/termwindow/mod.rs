cba1cfe533ab6e4c150375268094d30c08471eb8
kumattau <kumattau@gmail.com>
2022-05-11 23:32:57 +0900

    Fix text cursor position when showing tab bar

--- wezterm-gui/src/termwindow/mod.rs
+++ wezterm-gui/src/termwindow/mod.rs
@@ -1733,8 +1733,12 @@ impl TermWindow {
     fn update_text_cursor(&mut self, pane: &Rc<dyn Pane>) {
         let cursor = pane.get_cursor_position();
         if let Some(win) = self.window.as_ref() {
-            let top = pane.get_dimensions().physical_top + if self.show_tab_bar { -1 } else { 0 };
-
+            let top = pane.get_dimensions().physical_top;
+            let tab_bar_height = if self.show_tab_bar {
+                self.tab_bar_pixel_height().unwrap()
+            } else {
+                0.0
+            };
             let (padding_left, padding_top) = self.padding_left_top();
 
             let r = Rect::new(
@@ -1742,6 +1746,7 @@ impl TermWindow {
                     (cursor.x.max(0) as isize * self.render_metrics.cell_size.width)
                         .add(padding_left as isize),
                     ((cursor.y - top).max(0) as isize * self.render_metrics.cell_size.height)
+                        .add(tab_bar_height as isize)
                         .add(padding_top as isize),
                 ),
                 self.render_metrics.cell_size,
