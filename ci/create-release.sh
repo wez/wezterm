241e19babf29bdd13881b67988e5879e90e8532e
Wez Furlong <wez@wezfurlong.org>
2022-06-28 12:43:45 -0700

    ci: pre-fill release info, switch away from creating as a draft

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 name="$1"
-gh release view "$name" || gh release create --prerelease --draft "$name"
+gh release view "$name" || gh release create --prerelease --notes "See https://wezfurlong.org/wezterm/changelog.html#$name for the changelog" --title "$name" "$name"
