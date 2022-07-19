713450156ca72417bada2ecfb82ee38a5c44d961
Wez Furlong <wez@wezfurlong.org>
2022-07-19 06:38:50 -0700

    docs: only try to format tracked files
    Don't format generated docs; their inputs should be formatted instead.
    
    refs: https://github.com/wez/wezterm/pull/2273
    refs: https://github.com/wez/wezterm/issues/2253

--- ci/build-docs.sh
+++ ci/build-docs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+tracked_markdown=$(git ls-tree -r HEAD --name-only docs | egrep '\.(markdown|md)$')
+
+gelatyx lua --file $tracked_markdown --language-config ci/stylua.toml
+gelatyx lua --file $tracked_markdown --language-config ci/stylua.toml --check || exit 1
+
 set -x
 
 [[ -f /tmp/wezterm.releases.json ]] || curl https://api.github.com/repos/wez/wezterm/releases > /tmp/wezterm.releases.json
@@ -7,8 +13,6 @@ python3 ci/subst-release-info.py || exit 1
 python3 ci/generate-docs.py || exit 1
 mdbook-mermaid install docs
 mdbook build docs
-gelatyx lua --file docs/**/*.md --language-config ci/stylua.toml
-gelatyx lua --file docs/**/*.md --language-config ci/stylua.toml --check
 
 rm gh_pages/html/README.markdown
 cp assets/fonts/Symbols-Nerd-Font-Mono.ttf gh_pages/html/fonts/
