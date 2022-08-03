c0ebd2d131da9bd111982fbea582e3fcf83e77c0
Wez Furlong <wez@wezfurlong.org>
2022-08-03 06:45:35 -0700

    flatpak: refine build; install more executables in image

--- ci/flatpak.sh
+++ ci/flatpak.sh
@@ -7,4 +7,4 @@ flatpak install --noninteractive --user flathub org.freedesktop.Platform//21.08 
 python3 -m pip install toml aiohttp
 curl -L 'https://github.com/flatpak/flatpak-builder-tools/raw/master/cargo/flatpak-cargo-generator.py' > /tmp/flatpak-cargo-generator.py
 python3 /tmp/flatpak-cargo-generator.py Cargo.lock -o assets/flatpak/generated-sources.json
-flatpak-builder --install repo assets/flatpak/org.wezfurlong.wezterm.json --force-clean --user -y
+flatpak-builder --state-dir /var/tmp/wezterm-flatpak-builder --install /var/tmp/wezterm-flatpak-repo assets/flatpak/org.wezfurlong.wezterm.json --force-clean --user -y
