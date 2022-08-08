8b8d8f61e63af2b698ab58aac0ff2b5726c9bbdf
Wez Furlong <wez@wezfurlong.org>
2022-08-08 06:29:10 -0700

    add content rating and release info to appdata
    Required for flathub submission
    
    refs: https://github.com/flathub/flathub/pull/3398

--- ci/flatpak.sh
+++ ci/flatpak.sh
@@ -4,6 +4,9 @@ set -xe
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --noninteractive --user flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08 org.freedesktop.Sdk.Extension.rust-stable//21.08
 
+flatpak install --noninteractive --user org.freedesktop.appstream-glib
+flatpak run --env=G_DEBUG=fatal-criticals org.freedesktop.appstream-glib validate assets/wezterm.appdata.xml
+
 python3 -m pip install toml aiohttp
 curl -L 'https://github.com/flatpak/flatpak-builder-tools/raw/master/cargo/flatpak-cargo-generator.py' > /tmp/flatpak-cargo-generator.py
 python3 /tmp/flatpak-cargo-generator.py Cargo.lock -o assets/flatpak/generated-sources.json
