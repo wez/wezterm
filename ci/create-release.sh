ee436437b4294724c950602057ddd2a4aaaa7a55
Wez Furlong <wez@wezfurlong.org>
2022-09-04 08:31:57 -0700

    ci: expand auto-generated release description

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -x
 name="$1"
-gh release view "$name" || gh release create --prerelease --notes "See https://wezfurlong.org/wezterm/changelog.html#$name for the changelog" --title "$name" "$name"
+
+notes=$(cat <<'EOT'
+See https://wezfurlong.org/wezterm/changelog.html#$name for the changelog
+
+If you're looking for nightly downloads or more detailed installation instructions:
+
+[Windows](https://wezfurlong.org/wezterm/install/windows.html)
+[macOS](https://wezfurlong.org/wezterm/install/macos.html)
+[Linux](https://wezfurlong.org/wezterm/install/linux.html)
+[FreeBSD](https://wezfurlong.org/wezterm/install/freebsd.html)
+EOT
+)
+
+gh release view "$name" || gh release create --prerelease --notes "$notes" --title "$name" "$name"
