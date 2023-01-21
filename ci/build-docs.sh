ca7024aee3d52c5b8b8894536c07e6a6b154a204
Wez Furlong <wez@wezfurlong.org>
2023-01-21 06:38:38 -0700

    ci: try harder to successfully build the docs on PRs

--- ci/build-docs.sh
+++ ci/build-docs.sh
@@ -8,9 +8,12 @@ gelatyx lua --file $tracked_markdown --language-config ci/stylua.toml --check ||
 set -x
 
 # Use the GH CLI to make an authenticated request if available,
-# otherwise just do an ad-hoc curl
+# otherwise just do an ad-hoc curl.
+# However, if we are called from within a GH actions workflow (BUILD_REASON
+# is set), only use `gh` if GH_TOKEN is also set, otherwise it will refuse
+# to run.
 function ghapi() {
-  if hash gh 2>/dev/null ; then
+  if hash gh 2>/dev/null && test \( -n "$BUILD_REASON" -a -n "$GH_TOKEN" \) -o -z "$BUILD_REASON"; then
     gh api $1
   else
     curl https://api.github.com$1
