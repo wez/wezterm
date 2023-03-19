060d0f1ed479540d4caac44ea702c27c070299f3
Wez Furlong <wez@wezfurlong.org>
2023-03-19 13:12:20 -0700

    remove open dependency
    refs: https://github.com/wez/wezterm/issues/3288

--- wezterm-open-url/src/lib.rs
+++ wezterm-open-url/src/lib.rs
@@ -1,14 +1,57 @@
+// Portions of this file are derived from code that is
+// Copyright © 2015 Sebastian Thiel
+// <https://github.com/Byron/open-rs>
+
 #[cfg(not(windows))]
 pub fn open_url(url: &str) {
     let url = url.to_string();
     std::thread::spawn(move || {
-        let _ = open::that(&url);
+        #[cfg(target_os = "macos")]
+        let candidates: &[&[&str]] = &[&["/usr/bin/open", &url]];
+
+        #[cfg(not(target_os = "macos"))]
+        let candidates: &[&[&str]] = &[
+            &["xdg-open", &url],
+            &["gio", "open", &url] as &[_],
+            &["gnome-open", &url],
+            &["kde-open", &url],
+            &["wslview", &url],
+        ];
+
+        for candidate in candidates {
+            let mut cmd = std::process::Command::new(candidate[0]);
+            cmd.args(&candidate[1..]);
+
+            if let Ok(status) = cmd.status() {
+                if status.success() {
+                    return;
+                }
+            }
+        }
     });
 }
 
 #[cfg(not(windows))]
 pub fn open_with(url: &str, app: &str) {
-    open::with_in_background(url, app);
+    let url = url.to_string();
+    let app = app.to_string();
+
+    std::thread::spawn(move || {
+        #[cfg(target_os = "macos")]
+        let args: &[&str] = &["/usr/bin/open", "-a", &app, &url];
+
+        #[cfg(not(target_os = "macos"))]
+        let args: &[&str] = &[&app, &url];
+
+        let mut cmd = std::process::Command::new(args[0]);
+        cmd.args(&args[1..]);
+
+        if let Ok(status) = cmd.status() {
+            if status.success() {
+                return;
+            }
+        }
+    });
 }
 
 #[cfg(windows)]
