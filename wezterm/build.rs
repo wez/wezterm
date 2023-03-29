809288614681fc3a20f030260c7b4b583874b620
Wez Furlong <wez@wezfurlong.org>
2023-03-28 19:18:48 -0700

    windows: add manifest to config-using console apps, force UTF8 ACP
    Lua appears to populate package.path to something based on the
    executable path on windows, but since it uses msvcrt in ANSI mode,
    that string is encoded in whatever 8-bit MBCS is configured by
    the host system ACP setting.
    
    Rust expects that to be UTF-8, but Windows doesn't guarantee it.
    
    This commit updates the manifest for wezterm-gui to tell Windows
    that it wants its ACP to be set to UTF-8 prior to launch, which
    should resolve this situation for the GUI.
    
    This commit also introduces a more cut-down manifest for the
    console-subsystem executables that also use the lua config layer,
    which should hopefully resolve this issue for them.
    
    This commit was authored on a mac, so fingers crossed that it
    even compiles properly on windows!
    
    refs: https://github.com/wez/wezterm/issues/3390

---
+++ wezterm/build.rs
@@ -0,0 +1,30 @@
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    #[cfg(windows)]
+    {
+        let rcfile_name = Path::new(&std::env::var_os("OUT_DIR").unwrap()).join("resource.rc");
+        let mut rcfile = std::fs::File::create(&rcfile_name).unwrap();
+        write!(
+            rcfile,
+            r#"
+#include <winres.h>
+1 RT_MANIFEST "{win}\\console.manifest"
+"#,
+            win = windows_dir.display().to_string().replace("\\", "\\\\"),
+            version = version,
+        )
+        .unwrap();
+        drop(rcfile);
+
+        // Obtain MSVC environment so that the rc compiler can find the right headers.
+        // https://github.com/nabijaczleweli/rust-embed-resource/issues/11#issuecomment-603655972
+        let target = std::env::var("TARGET").unwrap();
+        if let Some(tool) = cc::windows_registry::find_tool(target.as_str(), "cl.exe") {
+            for (key, value) in tool.env() {
+                std::env::set_var(key, value);
+            }
+        }
+        embed_resource::compile(rcfile_name);
+    }
+}
