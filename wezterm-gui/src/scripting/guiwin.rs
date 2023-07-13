wez/wezterm

commit 750f49f0aef061ce82539b60e1a142a5f7943a4a

deps: upgrade mlua to 0.9.0-rc.1
refs: https://github.com/khvzak/mlua/issues/291
refs: #3849

wezterm-gui/src/scripting/guiwin.rs
@@ -4,7 +4,7 @@ use crate::termwindow::TermWindowNotif;
 use crate::TermWindow;
 use config::keyassignment::{ClipboardCopyDestination, KeyAssignment};
 use luahelper::*;
-use mlua::{UserData, UserDataMethods};
+use mlua::{UserData, UserDataMethods, UserDataRef};
 use mux::pane::PaneId;
 use mux::window::WindowId as MuxWindowId;
 use mux::Mux;
@@ -132,7 +132,7 @@ impl UserData for GuiWin {
         });
         methods.add_async_method(
             "get_selection_text_for_pane",
-            |_, this, pane: MuxPane| async move {
+            |_, this, pane: UserDataRef<MuxPane>| async move {
                 let (tx, rx) = smol::channel::bounded(1);
                 this.window.notify(TermWindowNotif::GetSelectionForPane {
                     pane_id: pane.0,
@@ -158,11 +158,11 @@ impl UserData for GuiWin {
         });
         methods.add_async_method(
             "perform_action",
-            |_, this, (assignment, pane): (KeyAssignment, MuxPane)| async move {
+            |_, this, (assignment, pane): (UserDataRef<KeyAssignment>, UserDataRef<MuxPane>)| async move {
                 let (tx, rx) = smol::channel::bounded(1);
                 this.window.notify(TermWindowNotif::PerformAssignment {
                     pane_id: pane.0,
-                    assignment,
+                    assignment: assignment.clone(),
                     tx: Some(tx),
                 });
                 let result = rx.recv().await.map_err(mlua::Error::external)?;
@@ -310,7 +310,7 @@ impl UserData for GuiWin {
         );
         methods.add_async_method(
             "get_selection_escapes_for_pane",
-            |_, this, pane: MuxPane| async move {
+            |_, this, pane: UserDataRef<MuxPane>| async move {
                 let (tx, rx) = smol::channel::bounded(1);
                 let pane_id = pane.0;
                 this.window

config/Cargo.toml
@@ -23,7 +23,7 @@ lazy_static = "1.4"
 libc = "0.2"
 log = "0.4"
 luahelper = { path = "../luahelper" }
-mlua = {version="0.8.3", features=["vendored", "lua54", "async", "send"]}
+mlua = {version="0.9.0-rc.1", features=["vendored", "lua54", "async", "send"]}
 # file change notification
 notify = "5.0.0"
 once_cell = "1.8"

config/src/config.rs
@@ -1045,7 +1045,7 @@ impl Config {
                     // file. Note that we can't catch this happening for files that are
                     // imported via the lua require function.
                     lua.load(s.trim_start_matches('\u{FEFF}'))
-                        .set_name(p.to_string_lossy())?
+                        .set_name(p.to_string_lossy())
                         .eval_async(),
                 )?;
                 let config = Config::apply_overrides_to(&lua, config)?;
@@ -1138,7 +1138,7 @@ impl Config {
                 "#,
             );
             let chunk = lua.load(&code);
-            let chunk = chunk.set_name(&format!("--config {}={}", key, value))?;
+            let chunk = chunk.set_name(format!("--config {}={}", key, value));
             lua.globals().set("config", config.clone())?;
             log::debug!("Apply {}={} to config", key, value);
             config = chunk.eval()?;

config/src/lua.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 use anyhow::{anyhow, Context};
 use luahelper::{from_lua_value_dynamic, lua_value_to_dynamic, to_lua};
-use mlua::{FromLua, Lua, Table, ToLuaMulti, Value, Variadic};
+use mlua::{FromLua, IntoLuaMulti, Lua, Table, Value, Variadic};
 use ordered_float::NotNan;
 use portable_pty::CommandBuilder;
 use std::convert::TryFrom;
@@ -150,14 +150,11 @@ fn config_builder_new_index<'lua>(
                     for i in 1.. {
                         if let Some(debug) = lua.inspect_stack(i) {
                             let names = debug.names();
-                            let name = names.name.map(String::from_utf8_lossy);
-                            let name_what = names.name_what.map(String::from_utf8_lossy);
+                            let name = names.name;
+                            let name_what = names.name_what;
 
                             let dbg_source = debug.source();
-                            let source = dbg_source
-                                .source
-                                .and_then(|b| String::from_utf8(b.to_vec()).ok())
-                                .unwrap_or_default();
+                            let source = dbg_source.source.unwrap_or_default();
                             let func_name = match (name, name_what) {
                                 (Some(name), Some(name_what)) => {
                                     format!("{name_what} {name}")
@@ -278,7 +275,7 @@ package.searchers[2] = function(module)
 end
         "#,
         )
-        .set_name("=searcher")?
+        .set_name("=searcher")
         .eval()
         .context("replace package.searchers")?;
 
@@ -798,7 +795,7 @@ pub fn emit_sync_callback<'lua, A>(
     (name, args): (String, A),
 ) -> mlua::Result<mlua::Value<'lua>>
 where
-    A: ToLuaMulti<'lua>,
+    A: IntoLuaMulti<'lua>,
 {
     let decorated_name = format!("wezterm-event-{}", name);
     let tbl: mlua::Value = lua.named_registry_value(&decorated_name)?;
@@ -819,7 +816,7 @@ pub async fn emit_async_callback<'lua, A>(
     (name, args): (String, A),
 ) -> mlua::Result<mlua::Value<'lua>>
 where
-    A: ToLuaMulti<'lua>,
+    A: IntoLuaMulti<'lua>,
 {
     let decorated_name = format!("wezterm-event-{}", name);
     let tbl: mlua::Value = lua.named_registry_value(&decorated_name)?;

lua-api-crates/color-funcs/src/lib.rs
@@ -1,6 +1,6 @@
 use crate::schemes::base16::Base16Scheme;
 use crate::schemes::sexy::Sexy;
-use config::lua::mlua::{self, Lua, MetaMethod, UserData, UserDataMethods};
+use config::lua::mlua::{self, Lua, MetaMethod, UserData, UserDataMethods, UserDataRef};
 use config::lua::{get_or_create_module, get_or_create_sub_module};
 use config::{ColorSchemeFile, ColorSchemeMetaData, Gradient, Palette, RgbaColor, SrgbaTuple};
 
@@ -51,7 +51,7 @@ impl UserData for ColorWrap {
             let s: String = this.0.into();
             Ok(s)
         });
-        methods.add_meta_method(MetaMethod::Eq, |_, this, other: ColorWrap| {
+        methods.add_meta_method(MetaMethod::Eq, |_, this, other: UserDataRef<ColorWrap>| {
             Ok(this.0 == other.0)
         });
         methods.add_method("complement", |_, this, _: ()| Ok(this.complement()));
@@ -95,10 +95,11 @@ impl UserData for ColorWrap {
         });
         methods.add_method("hsla", |_, this, _: ()| Ok(this.0.to_hsla()));
         methods.add_method("laba", |_, this, _: ()| Ok(this.0.to_laba()));
-        methods.add_method("contrast_ratio", |_, this, other: ColorWrap| {
-            Ok(this.0.contrast_ratio(&other.0))
-        });
-        methods.add_method("delta_e", |_, this, other: ColorWrap| {
+        methods.add_method(
+            "contrast_ratio",
+            |_, this, other: UserDataRef<ColorWrap>| Ok(this.0.contrast_ratio(&other.0)),
+        );
+        methods.add_method("delta_e", |_, this, other: UserDataRef<ColorWrap>| {
             Ok(this.0.delta_e(&other.0))
         });
     }

lua-api-crates/json/src/lib.rs
@@ -1,5 +1,5 @@
 use config::lua::get_or_create_module;
-use config::lua::mlua::{self, Lua, ToLua, Value as LuaValue};
+use config::lua::mlua::{self, IntoLua, Lua, Value as LuaValue};
 use luahelper::lua_value_to_dynamic;
 use serde_json::{Map, Value as JValue};
 use std::collections::HashSet;
@@ -27,18 +27,18 @@ fn json_value_to_lua_value<'lua>(lua: &'lua Lua, value: JValue) -> mlua::Result<
                 }
             },
         },
-        JValue::String(s) => s.to_lua(lua)?,
+        JValue::String(s) => s.into_lua(lua)?,
         JValue::Array(arr) => {
-            let tbl = lua.create_table_with_capacity(arr.len() as i32, 0)?;
+            let tbl = lua.create_table_with_capacity(arr.len() as usize, 0)?;
             for (idx, value) in arr.into_iter().enumerate() {
                 tbl.set(idx + 1, json_value_to_lua_value(lua, value)?)?;
             }
             LuaValue::Table(tbl)
         }
         JValue::Object(map) => {
-            let tbl = lua.create_table_with_capacity(0, map.len() as i32)?;
+            let tbl = lua.create_table_with_capacity(0, map.len() as usize)?;
             for (key, value) in map.into_iter() {
-                let key = key.to_lua(lua)?;
+                let key = key.into_lua(lua)?;
                 let value = json_value_to_lua_value(lua, value)?;
                 tbl.set(key, value)?;
             }
@@ -111,9 +111,7 @@ fn lua_value_to_json_value(value: LuaValue, visited: &mut HashSet<usize>) -> mlu
         }
         LuaValue::UserData(ud) => match ud.get_metatable() {
             Ok(mt) => {
-                if let Ok(to_dynamic) = mt.get::<mlua::MetaMethod, mlua::Function>(
-                    mlua::MetaMethod::Custom("__wezterm_to_dynamic".to_string()),
-                ) {
+                if let Ok(to_dynamic) = mt.get::<mlua::Function>("__wezterm_to_dynamic") {
                     match to_dynamic.call(LuaValue::UserData(ud.clone())) {
                         Ok(value) => {
                             let dyn_value = lua_value_to_dynamic(value)?;

lua-api-crates/mux/src/domain.rs
@@ -1,4 +1,5 @@
 use super::*;
+use mlua::UserDataRef;
 use mux::domain::{Domain, DomainId, DomainState};
 use std::sync::Arc;
 
@@ -27,16 +28,19 @@ impl UserData for MuxDomain {
             Ok(domain.spawnable())
         });
 
-        methods.add_async_method("attach", |_, this, window: Option<MuxWindow>| async move {
-            let mux = get_mux()?;
-            let domain = this.resolve(&mux)?;
-            domain.attach(window.map(|w| w.0)).await.map_err(|err| {
-                mlua::Error::external(format!(
-                    "failed to attach domain {}: {err:#}",
-                    domain.domain_name()
-                ))
-            })
-        });
+        methods.add_async_method(
+            "attach",
+            |_, this, window: Option<UserDataRef<MuxWindow>>| async move {
+                let mux = get_mux()?;
+                let domain = this.resolve(&mux)?;
+                domain.attach(window.map(|w| w.0)).await.map_err(|err| {
+                    mlua::Error::external(format!(
+                        "failed to attach domain {}: {err:#}",
+                        domain.domain_name()
+                    ))
+                })
+            },
+        );
 
         methods.add_method("detach", |_, this, _: ()| {
             let mux = get_mux()?;

lua-api-crates/mux/src/lib.rs
@@ -2,6 +2,7 @@ use config::keyassignment::SpawnTabDomain;
 use config::lua::mlua::{self, Lua, UserData, UserDataMethods, Value as LuaValue};
 use config::lua::{get_or_create_module, get_or_create_sub_module};
 use luahelper::impl_lua_conversion_dynamic;
+use mlua::UserDataRef;
 use mux::domain::{DomainId, SplitSource};
 use mux::pane::{Pane, PaneId};
 use mux::tab::{SplitDirection, SplitRequest, SplitSize, Tab, TabId};
@@ -159,7 +160,7 @@ pub fn register(lua: &Lua) -> anyhow::Result<()> {
 
     mux_mod.set(
         "set_default_domain",
-        lua.create_function(|_, domain: MuxDomain| {
+        lua.create_function(|_, domain: UserDataRef<MuxDomain>| {
             let mux = get_mux()?;
             let domain = domain.resolve(&mux)?;
             mux.set_default_domain(&domain);
@@ -179,8 +180,8 @@ struct CommandBuilderFrag {
 }
 
 impl CommandBuilderFrag {
-    fn to_command_builder(self) -> (Option<CommandBuilder>, Option<String>) {
-        if let Some(args) = self.args {
+    fn to_command_builder(&self) -> (Option<CommandBuilder>, Option<String>) {
+        if let Some(args) = &self.args {
             let mut builder = CommandBuilder::from_argv(args.iter().map(Into::into).collect());
             for (k, v) in self.set_environment_variables.iter() {
                 builder.env(k, v);
@@ -190,7 +191,7 @@ impl CommandBuilderFrag {
             }
             (Some(builder), None)
         } else {
-            (None, self.cwd)
+            (None, self.cwd.clone())
         }
     }
 }
@@ -273,7 +274,7 @@ struct SpawnTab {
 impl_lua_conversion_dynamic!(SpawnTab);
 
 impl SpawnTab {
-    async fn spawn(self, window: MuxWindow) -> mlua::Result<(MuxTab, MuxPane, MuxWindow)> {
+    async fn spawn(self, window: &MuxWindow) -> mlua::Result<(MuxTab, MuxPane, MuxWindow)> {
         let mux = get_mux()?;
         let size;
         let pane;

commit 31eb4fe27196c1dd993c569941f106db56af3016

fix window:perform_action
Accidentally broken by 750f49f0aef061ce82539b60e1a142a5f7943a4a

refs: https://github.com/wez/wezterm/issues/3981

wezterm-gui/src/scripting/guiwin.rs
@@ -158,11 +158,11 @@ impl UserData for GuiWin {
         });
         methods.add_async_method(
             "perform_action",
-            |_, this, (assignment, pane): (UserDataRef<KeyAssignment>, UserDataRef<MuxPane>)| async move {
+            |_, this, (assignment, pane): (KeyAssignment, UserDataRef<MuxPane>)| async move {
                 let (tx, rx) = smol::channel::bounded(1);
                 this.window.notify(TermWindowNotif::PerformAssignment {
                     pane_id: pane.0,
-                    assignment: assignment.clone(),
+                    assignment,
                     tx: Some(tx),
                 });
                 let result = rx.recv().await.map_err(mlua::Error::external)?;