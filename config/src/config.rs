1a6f2123129063a9aa46a274a3270bfa284c9bb0
UserIsntAvailable <pinedap.angel@gmail.com>
2023-08-18 19:08:38 -0400

    add `awesome` to default `tiling_desktop_environments`.

--- config/src/config.rs
+++ config/src/config.rs
@@ -1711,7 +1711,7 @@ fn default_max_fps() -> u8 {
 }
 
 fn default_tiling_desktop_environments() -> Vec<String> {
-    ["X11 LG3D", "X11 bspwm", "X11 i3", "X11 dwm"]
+    ["X11 LG3D", "X11 bspwm", "X11 i3", "X11 dwm", "X11 awesome"]
         .iter()
         .map(|s| s.to_string())
         .collect()
