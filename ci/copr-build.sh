f58e7b25d47b3380b302b69317dcd1ad155a2252
Wez Furlong <wez@wezfurlong.org>
2023-10-02 17:31:59 -0700

    copr: ensure submodules are checked out

--- ci/copr-build.sh
+++ ci/copr-build.sh
@@ -10,6 +10,7 @@ git config --global --add safe.directory $$PWD/deps/freetype/freetype2
 git config --global --add safe.directory $$PWD/deps/freetype/libpng
 git config --global --add safe.directory $$PWD/deps/freetype/zlib
 git config --global --add safe.directory $$PWD/deps/harfbuzz/harfbuzz
+git submodule update --init --recursive
 
 ./ci/source-archive.sh
 ./ci/deploy.sh
