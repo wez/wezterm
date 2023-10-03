99ea35f6f0381c23d75e4ccc0d474434a594c035
Wez Furlong <wez@wezfurlong.org>
2023-10-02 18:47:03 -0700

    copr: tweak git stuff some more

--- ci/copr-build.sh
+++ ci/copr-build.sh
@@ -5,12 +5,11 @@ set -e
 
 command -v git || dnf install -y git
 
-git config --global --add safe.directory $$PWD
-git config --global --add safe.directory $$PWD/deps/freetype/freetype2
-git config --global --add safe.directory $$PWD/deps/freetype/libpng
-git config --global --add safe.directory $$PWD/deps/freetype/zlib
-git config --global --add safe.directory $$PWD/deps/harfbuzz/harfbuzz
-git submodule update --init --recursive
+git config --global --add safe.directory $PWD
+git config --global --add safe.directory $PWD/deps/freetype/freetype2
+git config --global --add safe.directory $PWD/deps/freetype/libpng
+git config --global --add safe.directory $PWD/deps/freetype/zlib
+git config --global --add safe.directory $PWD/deps/harfbuzz/harfbuzz
 
 ./ci/source-archive.sh
 ./ci/deploy.sh
