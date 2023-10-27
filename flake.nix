b00321911281af5de6f6224e1b3096bbb58d3519
Hilmar Wiegand <me@hwgnd.de>
2023-10-27 16:11:59 +0200

    Update flake dependencies

--- flake.nix
+++ flake.nix
@@ -88,7 +88,6 @@
         cargoLock = {
           lockFile = ./Cargo.lock;
           outputHashes = {
-            "image-0.24.5" = "sha256-fTajVwm88OInqCPZerWcSAm1ga46ansQ3EzAmbT58Js=";
             "xcb-imdkit-0.2.0" = "sha256-L+NKD0rsCk9bFABQF4FZi9YoqBHr4VAZeKAWgsaAegw=";
             "xcb-1.2.1" = "sha256-zkuW5ATix3WXBAj2hzum1MJ5JTX3+uVQ01R1vL6F1rY=";
           };
