e42b523c22c553dd63b34dc79fa947726c4aee48
Danielkonge <dakongsgaard@gmail.com>
2023-12-01 18:14:21 +0100

    simple `to_string` with `ValuePrinter`

--- lua-api-crates/table-funcs/src/lib.rs
+++ lua-api-crates/table-funcs/src/lib.rs
@@ -1,6 +1,6 @@
-use anyhow::anyhow;
 use config::lua::get_or_create_sub_module;
 use config::lua::mlua::{self, Integer, Lua, Table, Value as LuaValue};
+use luahelper::ValuePrinter;
 
 pub fn register(lua: &Lua) -> anyhow::Result<()> {
     let table = get_or_create_sub_module(lua, "table")?;
@@ -114,89 +114,9 @@ fn to_string_fallback<'lua>(_: &'lua Lua, table: Table<'lua>) -> mlua::Result<St
 }
 
 fn to_string<'lua>(
-    lua: &'lua Lua,
-    (table, indent, skip_outer_bracket): (Table<'lua>, Option<i64>, Option<bool>),
+    _: &'lua Lua,
+    table: Table<'lua>,
 ) -> mlua::Result<String> {
-    if let Some(ind) = indent {
-        if ind < 0 {
-            return Err(mlua::Error::external(anyhow!(
-                "Indent set to {ind}. Please use an indent ≥ 0."
-            )));
-        }
-    }
-    let result = to_string_impl(lua, (table, indent, skip_outer_bracket, 0));
-    match result {
-        Ok(res) => {
-            if skip_outer_bracket == Some(true) {
-                // we added indent too many spaces on each line
-                let extra_spaces = match indent {
-                    Some(ind) => " ".repeat(ind as usize),
-                    None => " ".repeat(2),
-                };
-                let old = ["\n", &extra_spaces].concat();
-                return Ok(res.replace(&old, "\n"));
-            }
-            Ok(res)
-        }
-        Err(err) => Err(mlua::Error::external(err)),
-    }
-}
-
-fn to_string_impl<'lua>(
-    lua: &'lua Lua,
-    (table, indent, skip_outer_bracket, depth): (Table<'lua>, Option<i64>, Option<bool>, usize),
-) -> mlua::Result<String> {
-    let mut string = String::new();
-    let skip_outer_bracket = match skip_outer_bracket {
-        Some(b) => b,
-        None => false, // defaults to keeping the outer brackets
-    };
-    if !skip_outer_bracket || depth != 0 {
-        string.push_str("{\n");
-    }
-
-    let bracket_spaces = match indent {
-        Some(ind) => " ".repeat((ind as usize) * depth),
-        None => " ".repeat(2 * depth),
-    };
-    let content_spaces = match indent {
-        Some(ind) => " ".repeat((ind as usize) * (depth + 1)),
-        None => " ".repeat(2 * (depth + 1)),
-    };
-    for pair in table.pairs::<LuaValue, LuaValue>() {
-        string.push_str(&content_spaces);
-
-        let (key, value) = pair.map_err(mlua::Error::external)?;
-        match value.clone() {
-            LuaValue::Table(tbl) => {
-                string.push_str(&to_string_impl(lua, (tbl, indent, None, depth + 1))?)
-            }
-            _ => {
-                let nice_key = match key {
-                    LuaValue::String(s) => s.to_str().map_err(mlua::Error::external)?.to_string(),
-                    LuaValue::Number(f) => f.to_string(),
-                    LuaValue::Integer(i) => i.to_string(),
-                    LuaValue::Boolean(b) => b.to_string(),
-                    other => format!("{other:?}"),
-                };
-                let nice_value = match value {
-                    LuaValue::String(s) => s.to_str().map_err(mlua::Error::external)?.to_string(),
-                    LuaValue::Number(f) => f.to_string(),
-                    LuaValue::Integer(i) => i.to_string(),
-                    LuaValue::Boolean(b) => b.to_string(),
-                    other => format!("{other:?}"),
-                };
-                string.push_str(&format!("{nice_key} = {nice_value},\n"));
-            }
-        }
-    }
-    if depth != 0 {
-        string.push_str(&bracket_spaces);
-        string.push_str("},\n")
-    } else if skip_outer_bracket {
-        string.pop(); // remove the last newline in this case
-    } else {
-        string.push_str("}");
-    }
-    Ok(string)
+    let res = ValuePrinter(LuaValue::Table(table));
+    Ok(format!("{:#?}", res).to_string())
 }
