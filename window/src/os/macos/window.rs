2196c9ab491755ea69c48a54a55dcff034de1f17
Gabriel Nützi <gnuetzi@gmail.com>
2023-12-07 14:20:52 +0100

    fix: Add capslock on macOS to modifier state

--- window/src/os/macos/window.rs
+++ window/src/os/macos/window.rs
@@ -1754,6 +1754,9 @@ fn key_modifiers(flags: NSEventModifierFlags) -> Modifiers {
     if flags.contains(NSEventModifierFlags::NSCommandKeyMask) {
         mods |= Modifiers::SUPER;
     }
+    if flags.contains(NSEventModifierFlags::NSAlphaShiftKeyMask) {
+        mods |= Modifiers::CAPS_LOCK;
+    }
 
     mods
 }
