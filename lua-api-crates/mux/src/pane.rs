wez/wezterm

commit f71bce1727bbf1c384ca63a650a2aadec5afe841

mux: propagate pane focus change events to clients
This commit causes the mux to generate a PaneFocused notification
when the active pane is changed.

The mux server will forward that as a unilateral PDU to connected
clients.

The clientpane implementation will handle that by applying the
same state to the local mux.

refs: #2863

mux/src/lib.rs
@@ -77,6 +77,7 @@ pub enum MuxNotification {
         tab_id: TabId,
         window_id: WindowId,
     },
+    PaneFocused(PaneId),
 }
 
 static SUB_ID: AtomicUsize = AtomicUsize::new(0);

mux/src/tab.rs
@@ -1715,14 +1715,17 @@ impl TabInner {
     }
 
     fn advise_focus_change(&mut self, prior: Option<Arc<dyn Pane>>) {
+        let mux = Mux::get();
         let current = self.get_active_pane();
         match (prior, current) {
             (Some(prior), Some(current)) if prior.pane_id() != current.pane_id() => {
                 prior.focus_changed(false);
                 current.focus_changed(true);
+                mux.notify(MuxNotification::PaneFocused(current.pane_id()));
             }
             (None, Some(current)) => {
                 current.focus_changed(true);
+                mux.notify(MuxNotification::PaneFocused(current.pane_id()));
             }
             (Some(prior), None) => {
                 prior.focus_changed(false);

wezterm-mux-server-impl/src/sessionhandler.rs
@@ -301,6 +301,7 @@ impl SessionHandler {
                     let mux = Mux::get();
                     let _identity = mux.with_identity(client_id);
                     mux.record_focus_for_current_identity(pane_id);
+                    mux.notify(mux::MuxNotification::PaneFocused(pane_id));
                 })
                 .detach();
                 send_response(Ok(Pdu::UnitResponse(UnitResponse {})))
@@ -746,6 +747,7 @@ impl SessionHandler {
             | Pdu::GetTlsCredsResponse { .. }
             | Pdu::GetClientListResponse { .. }
             | Pdu::PaneRemoved { .. }
+            | Pdu::PaneFocused { .. }
             | Pdu::GetImageCellResponse { .. }
             | Pdu::MovePaneToNewTabResponse { .. }
             | Pdu::GetPaneRenderableDimensionsResponse { .. }

codec/src/lib.rs
@@ -417,7 +417,7 @@ macro_rules! pdu {
 /// The overall version of the codec.
 /// This must be bumped when backwards incompatible changes
 /// are made to the types and protocol.
-pub const CODEC_VERSION: usize = 33;
+pub const CODEC_VERSION: usize = 34;
 
 // Defines the Pdu enum.
 // Each struct has an explicit identifying number.
@@ -468,6 +468,7 @@ pdu! {
     ActivatePaneDirection: 50,
     GetPaneRenderableDimensions: 51,
     GetPaneRenderableDimensionsResponse: 52,
+    PaneFocused: 53,
 }
 
 impl Pdu {
@@ -543,6 +544,7 @@ impl Pdu {
             | Pdu::SetPalette(SetPalette { pane_id, .. })
             | Pdu::NotifyAlert(NotifyAlert { pane_id, .. })
             | Pdu::SetClipboard(SetClipboard { pane_id, .. })
+            | Pdu::PaneFocused(PaneFocused { pane_id })
             | Pdu::PaneRemoved(PaneRemoved { pane_id }) => Some(*pane_id),
             _ => None,
         }
@@ -731,6 +733,11 @@ pub struct SetPalette {
 pub struct NotifyAlert {
     pub pane_id: PaneId,
     pub alert: Alert,
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+pub struct PaneFocused {
+    pub pane_id: PaneId,
 }
 
 #[derive(Deserialize, Serialize, PartialEq, Debug)]

wezterm-client/src/pane/clientpane.rs
@@ -169,6 +169,45 @@ impl ClientPane {
 
                 self.client.expire_stale_mappings();
             }
+            Pdu::PaneFocused(PaneFocused { pane_id }) => {
+                // We get here whenever the pane focus is changed on the
+                // server. That might be due to the user here in the GUI
+                // doing things, or it may be due to a "remote"
+                // `wezterm cli activate-pane-direction` or similar call
+                // from some other actor.
+                // The latter case is the important one: it is desirable
+                // for the focus change to be reflected locally after it
+                // has been changed on the server, so we work to apply
+                // it here.
+                log::trace!("advised of remote pane focus: {pane_id}");
+
+                let mux = Mux::get();
+                let (_domain, window_id, tab_id) = mux
+                    .resolve_pane_id(self.local_pane_id)
+                    .ok_or_else(|| anyhow::anyhow!("can't find myself in the mux"))?;
+
+                // Focus/activate the containing tab within its window
+                {
+                    let mut win = mux
+                        .get_window_mut(window_id)
+                        .ok_or_else(|| anyhow::anyhow!("window_id {window_id} not found"))?;
+                    let tab_idx = win
+                        .idx_by_id(tab_id)
+                        .ok_or_else(|| anyhow::anyhow!("tab {tab_id} not in {window_id}"))?;
+                    win.save_and_then_set_active(tab_idx);
+                }
+
+                // Focus/activate the pane locally
+                let tab = mux
+                    .get_tab(tab_id)
+                    .ok_or_else(|| anyhow::anyhow!("tab {tab_id} not found"))?;
+
+                let pane = mux.get_pane(self.local_pane_id).ok_or_else(|| {
+                    anyhow::anyhow!("pane {} not found. That's me?", self.local_pane_id)
+                })?;
+
+                tab.set_active_pane(&pane);
+            }
             _ => bail!("unhandled unilateral pdu: {:?}", pdu),
         };
         Ok(())

wezterm-gui/src/frontend.rs
@@ -64,6 +64,7 @@ impl GuiFrontEnd {
                     })
                     .detach();
                 }
+                MuxNotification::PaneFocused(_) => {}
                 MuxNotification::TabAddedToWindow { .. } => {}
                 MuxNotification::PaneRemoved(_) => {}
                 MuxNotification::WindowInvalidated(_) => {}

wezterm-gui/src/termwindow/mod.rs
@@ -1161,6 +1161,9 @@ impl TermWindow {
                 MuxNotification::SaveToDownloads { .. } => {
                     // Handled by frontend
                 }
+                MuxNotification::PaneFocused(_) => {
+                    // Handled by frontend
+                }
                 MuxNotification::PaneAdded(_)
                 | MuxNotification::PaneRemoved(_)
                 | MuxNotification::WindowWorkspaceChanged(_)
@@ -1345,6 +1348,7 @@ impl TermWindow {
             }
             | MuxNotification::AssignClipboard { .. }
             | MuxNotification::SaveToDownloads { .. }
+            | MuxNotification::PaneFocused(_)
             | MuxNotification::PaneRemoved(_)
             | MuxNotification::WindowCreated(_)
             | MuxNotification::ActiveWorkspaceChanged(_)

wezterm-mux-server-impl/src/dispatch.rs
@@ -159,6 +159,12 @@ where
                     stream.flush().await.context("flushing PDU to client")?;
                 }
             }
+            Ok(Item::Notif(MuxNotification::PaneFocused(pane_id))) => {
+                Pdu::PaneFocused(codec::PaneFocused { pane_id })
+                    .encode_async(&mut stream, 0)
+                    .await?;
+                stream.flush().await.context("flushing PDU to client")?;
+            }
             Ok(Item::Notif(MuxNotification::ActiveWorkspaceChanged(_))) => {}
             Ok(Item::Notif(MuxNotification::Empty)) => {}
             Err(err) => {

commit 2f7ca41a5e6aece60abd58c1febfe2fe405edd3d

Fix pane focus rapidly switching back and forth
Switching panes multiple times too quickly sometimes causes the focus to
get stuck rapidly switching back and forth between two panes by itself.
This happens both locally and on an SSH domain. The root cause is
similar in both cases.

In the local case, GuiFrontend handles a MuxNotification::PaneFocused
event by calling Mux::focus_pane_and_containing_tab, which calls through
Tab::set_active_pane -> TabInner::set_active_pane ->
TabInner::advise_focus_change. TabInner::advise_focus_change then calls
Mux::notify with a new MuxNotification::PaneFocused event, which is a
redundant notification focusing the same pane again.

This is normally harmless other than a small amount of wasted work;
TabInner::set_active_pane notices that the focused pane didn't change
and doesn't generate yet another event.

However, if another real focus change is queued between the original
focus change and the redundant one, we get into trouble. For example, if
the user switches to pane 0 and then immediately to pane 1, the event
queue contains [PaneFocused(0), PaneFocused(1)]. When the first event is
handled, pane 0 is focused, and a redundant notification is generated,
so the event queue contains [PaneFocused(1), PaneFocused(0)]. Then after
the next event is handled, pane 1 is focused, and we add another
redundant notification, so the event queue contains [PaneFocused(0),
PaneFocused(1)]. Repeat ad nauseam.

Fix this by not generating the notification from
TabInner::advise_focus_change when it would be redundant, which we
indicate with a new boolean parameter.

The mux server case is very similar, except that Pdu::SetFocusedPane in
SessionHandler is the vector for the bug.

This bug appears to have been introduced by commit
f71bce1727bbf1c384ca63a650a2aadec5afe841. I verified that `wezterm cli
activate-pane-direction` still works over SSH after this fix.

closes: #4390
closes: #4693

lua-api-crates/mux/src/pane.rs
@@ -1,6 +1,7 @@
 use super::*;
 use luahelper::{dynamic_to_lua_value, from_lua, to_lua};
 use mlua::Value;
+use mux::tab::NotifyMux;
 use std::cmp::Ordering;
 use std::sync::Arc;
 use termwiz::cell::SemanticType;
@@ -400,7 +401,7 @@ impl UserData for MuxPane {
             let tab = mux
                 .get_tab(tab_id)
                 .ok_or_else(|| mlua::Error::external(format!("tab {tab_id} not found")))?;
-            tab.set_active_pane(&pane);
+            tab.set_active_pane(&pane, NotifyMux::Yes);
             Ok(())
         });
 

mux/src/lib.rs
@@ -1,6 +1,6 @@
 use crate::client::{ClientId, ClientInfo};
 use crate::pane::{Pane, PaneId};
-use crate::tab::{SplitRequest, Tab, TabId};
+use crate::tab::{NotifyMux, SplitRequest, Tab, TabId};
 use crate::window::{Window, WindowId};
 use anyhow::{anyhow, Context, Error};
 use config::keyassignment::SpawnTabDomain;
@@ -542,7 +542,7 @@ impl Mux {
             .get_tab(tab_id)
             .ok_or_else(|| anyhow::anyhow!("tab {tab_id} not found"))?;
 
-        tab.set_active_pane(&pane);
+        tab.set_active_pane(&pane, NotifyMux::No);
 
         Ok(())
     }

mux/src/tab.rs
@@ -20,6 +20,12 @@ pub type Cursor = bintree::Cursor<Arc<dyn Pane>, SplitDirectionAndSize>;
 static TAB_ID: ::std::sync::atomic::AtomicUsize = ::std::sync::atomic::AtomicUsize::new(0);
 pub type TabId = usize;
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum NotifyMux {
+    Yes,
+    No,
+}
+
 #[derive(Default)]
 struct Recency {
     count: usize,
@@ -698,8 +704,8 @@ impl Tab {
         self.inner.lock().get_active_idx()
     }
 
-    pub fn set_active_pane(&self, pane: &Arc<dyn Pane>) {
-        self.inner.lock().set_active_pane(pane)
+    pub fn set_active_pane(&self, pane: &Arc<dyn Pane>, notify: NotifyMux) {
+        self.inner.lock().set_active_pane(pane, notify)
     }
 
     pub fn set_active_idx(&self, pane_index: usize) {
@@ -1747,7 +1753,7 @@ impl TabInner {
         self.active
     }
 
-    fn set_active_pane(&mut self, pane: &Arc<dyn Pane>) {
+    fn set_active_pane(&mut self, pane: &Arc<dyn Pane>, notify: NotifyMux) {
         if let Some(item) = self
             .iter_panes_ignoring_zoom()
             .iter()
@@ -1756,22 +1762,26 @@ impl TabInner {
             let prior = self.get_active_pane();
             self.active = item.index;
             self.recency.tag(item.index);
-            self.advise_focus_change(prior);
+            self.advise_focus_change(prior, notify);
         }
     }
 
-    fn advise_focus_change(&mut self, prior: Option<Arc<dyn Pane>>) {
+    fn advise_focus_change(&mut self, prior: Option<Arc<dyn Pane>>, notify: NotifyMux) {
         let mux = Mux::get();
         let current = self.get_active_pane();
         match (prior, current) {
             (Some(prior), Some(current)) if prior.pane_id() != current.pane_id() => {
                 prior.focus_changed(false);
                 current.focus_changed(true);
-                mux.notify(MuxNotification::PaneFocused(current.pane_id()));
+                if notify == NotifyMux::Yes {
+                    mux.notify(MuxNotification::PaneFocused(current.pane_id()));
+                }
             }
             (None, Some(current)) => {
                 current.focus_changed(true);
-                mux.notify(MuxNotification::PaneFocused(current.pane_id()));
+                if notify == NotifyMux::Yes {
+                    mux.notify(MuxNotification::PaneFocused(current.pane_id()));
+                }
             }
             (Some(prior), None) => {
                 prior.focus_changed(false);
@@ -1786,7 +1796,7 @@ impl TabInner {
         let prior = self.get_active_pane();
         self.active = pane_index;
         self.recency.tag(pane_index);
-        self.advise_focus_change(prior);
+        self.advise_focus_change(prior, NotifyMux::Yes);
     }
 
     fn assign_pane(&mut self, pane: &Arc<dyn Pane>) {
@@ -1849,7 +1859,7 @@ impl TabInner {
         if keep_focus {
             self.set_active_idx(pane_index);
         } else {
-            self.advise_focus_change(Some(pane));
+            self.advise_focus_change(Some(pane), NotifyMux::Yes);
         }
         None
     }

wezterm-mux-server-impl/src/sessionhandler.rs
@@ -6,7 +6,7 @@ use mux::client::ClientId;
 use mux::domain::SplitSource;
 use mux::pane::{Pane, PaneId};
 use mux::renderable::{RenderableDimensions, StableCursorPosition};
-use mux::tab::TabId;
+use mux::tab::{NotifyMux, TabId};
 use mux::{Mux, MuxNotification};
 use promise::spawn::spawn_into_main_thread;
 use std::collections::HashMap;
@@ -332,10 +332,9 @@ impl SessionHandler {
                             let tab = mux
                                 .get_tab(tab_id)
                                 .ok_or_else(|| anyhow::anyhow!("tab {tab_id} not found"))?;
-                            tab.set_active_pane(&pane);
+                            tab.set_active_pane(&pane, NotifyMux::No);
 
                             mux.record_focus_for_current_identity(pane_id);
-                            mux.notify(mux::MuxNotification::PaneFocused(pane_id));
 
                             Ok(Pdu::UnitResponse(UnitResponse {}))
                         },
@@ -538,14 +537,14 @@ impl SessionHandler {
                                     if is_zoomed != zoomed {
                                         tab.set_zoomed(false);
                                         if zoomed {
-                                            tab.set_active_pane(&pane);
+                                            tab.set_active_pane(&pane, NotifyMux::Yes);
                                             tab.set_zoomed(zoomed);
                                         }
                                     }
                                 }
                                 None => {
                                     if zoomed {
-                                        tab.set_active_pane(&pane);
+                                        tab.set_active_pane(&pane, NotifyMux::Yes);
                                         tab.set_zoomed(zoomed);
                                     }
                                 }