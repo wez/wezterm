2f7484714d0e6cef4c4c4110b41351f8e3825b6e
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2024-01-03 20:22:07 -0500

    Nuke Wayland

--- window/src/os/wayland/mod.rs
+++ window/src/os/wayland/mod.rs
@@ -1,26 +1,27 @@
 #![cfg(all(unix, not(target_os = "macos")))]
 
 pub mod connection;
-pub mod inputhandler;
-pub mod output;
-pub mod window;
-pub use self::window::*;
+// pub mod inputhandler;
+// pub mod output;
+// pub mod window;
+// pub use self::window::*;
 pub use connection::*;
-pub use output::*;
-mod copy_and_paste;
-mod drag_and_drop;
-mod frame;
-mod pointer;
+// pub use output::*;
+// mod copy_and_paste;
+// mod drag_and_drop;
+// mod frame;
+// mod pointer;
 
 /// Returns the id of a wayland proxy object, suitable for using
 /// a key into hash maps
-pub fn wl_id<I, T>(obj: T) -> u32
-where
-    I: wayland_client::Interface,
-    T: AsRef<wayland_client::Proxy<I>>,
-    I: AsRef<wayland_client::Proxy<I>>,
-    I: From<wayland_client::Proxy<I>>,
-{
-    let proxy: &wayland_client::Proxy<I> = obj.as_ref();
-    proxy.id()
-}
+pub fn todo() {}
+// pub fn wl_id<I, T>(obj: T) -> u32
+// where
+//     I: wayland_client::Interface,
+//     T: AsRef<wayland_client::Proxy<I>>,
+//     I: AsRef<wayland_client::Proxy<I>>,
+//     I: From<wayland_client::Proxy<I>>,
+// {
+//     let proxy: &wayland_client::Proxy<I> = obj.as_ref();
+//     proxy.id()
+// }
