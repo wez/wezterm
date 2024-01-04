3308cb578bba24d290f2a8d1e7dd2c14f8bfaefe
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2024-01-03 20:28:32 -0500

    Connections compile without complaining; time for window

--- window/src/os/wayland/connection.rs
+++ window/src/os/wayland/connection.rs
@@ -1,1 +1,25 @@
+use crate::ConnectionOps;
+
 pub struct WaylandConnection {}
+
+impl WaylandConnection {
+    pub(crate) fn create_new() -> anyhow::Result<Self> {
+        Ok( WaylandConnection{} )
+    }
+
+    pub(crate) fn advise_of_appearance_change(&self, appearance: crate::Appearance) {}
+}
+
+impl ConnectionOps for WaylandConnection {
+    fn name(&self) -> String {
+        todo!()
+    }
+
+    fn terminate_message_loop(&self) {
+        todo!()
+    }
+
+    fn run_message_loop(&self) -> anyhow::Result<()> {
+        todo!()
+    }
+}
