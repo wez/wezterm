cfa6231ac91b5da00ec46da012bbef4cee1a6224
Mehul Kar <mehul.kar@vercel.com>
2024-01-08 21:04:45 -0600

    expose as_command API on CommandBuilder publicly

--- pty/src/cmdbuilder.rs
+++ pty/src/cmdbuilder.rs
@@ -474,7 +474,7 @@ impl CommandBuilder {
     }
 
     /// Convert the CommandBuilder to a `std::process::Command` instance.
-    pub(crate) fn as_command(&self) -> anyhow::Result<std::process::Command> {
+    pub fn as_command(&self) -> anyhow::Result<std::process::Command> {
         use std::os::unix::process::CommandExt;
 
         let home = self.get_home_dir()?;
