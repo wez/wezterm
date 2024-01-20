73db71cf1c1ffa8534e267280392604e890354d4
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2024-01-19 17:27:32 -0800

    Mouse seems to do something, but it is incorrect

--- window/src/os/wayland/pointer.rs
+++ window/src/os/wayland/pointer.rs
@@ -1,48 +1,182 @@
 use std::cell::RefCell;
-use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
-use smithay_client_toolkit::seat::pointer::{PointerEvent, PointerEventKind, PointerHandler};
+use smithay_client_toolkit::seat::pointer::{
+    PointerData, PointerDataExt, PointerEvent, PointerEventKind, PointerHandler,
+};
+use wayland_client::backend::ObjectId;
 use wayland_client::protocol::wl_pointer::{ButtonState, WlPointer};
+use wayland_client::protocol::wl_seat::WlSeat;
 use wayland_client::{Connection, Proxy, QueueHandle};
 use wezterm_input_types::MousePress;
 
+use crate::ConnectionOps;
+
 use super::state::WaylandState;
+use super::WaylandConnection;
 
 impl PointerHandler for WaylandState {
     fn pointer_frame(
         &mut self,
-        conn: &Connection,
-        qh: &QueueHandle<Self>,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
         pointer: &WlPointer,
         events: &[PointerEvent],
     ) {
+        let mut pstate = pointer
+            .data::<PointerUserData>()
+            .unwrap()
+            .state
+            .lock()
+            .unwrap();
+
         for evt in events {
             if let PointerEventKind::Enter { .. } = &evt.kind {
                 let surface_id = evt.surface.id();
-                self.active_surface_id = RefCell::new(Some(surface_id));
+                self.active_surface_id = RefCell::new(Some(surface_id.clone()));
+                pstate.active_surface_id = Some(surface_id);
             }
-            if let Some(serial) = event_serial(&evt) {}
+            if let Some(serial) = event_serial(&evt) {
+                *self.last_serial.borrow_mut() = serial;
+                pstate.serial = serial;
+            }
+            if let Some(pending) = self
+                .surface_to_pending
+                .get(&self.active_surface_id.borrow().as_ref().unwrap())
+            {
+                let mut pending = pending.lock().unwrap();
+                if pending.queue(evt) {
+                    WaylandConnection::with_window_inner(pending.window_id, move |inner| {
+                        inner.dispatch_pending_mouse();
+                        Ok(())
+                    });
+                }
+            }
         }
     }
 }
 
-// pub(super) struct PointerData {
-//     active_surface_id: u32,
-//     surface_to_pending: HashMap<u32, Arc<Mutex<PendingMouse>>>,
-//     // TODO: drag_and_drop: DragAndDrop,
-//     serial: u32,
-// }
-//
-// #[derive(Clone, Debug)]
-// pub struct PendingMouse {
-//     window_id: usize,
-//     // TODO: copy_and_paste: Arc<Mutex<CopyAndPaste>>,
-//     surface_coords: Option<(f64, f64)>,
-//     button: Vec<(MousePress, ButtonState)>,
-//     scroll: Option<(f64, f64)>,
-//     in_window: bool,
-// }
+#[derive(Debug)]
+pub(super) struct PointerUserData {
+    pdata: PointerData,
+    state: Mutex<PointerState>,
+}
+
+impl PointerUserData {
+    pub(super) fn new(seat: WlSeat) -> Self {
+        Self {
+            pdata: PointerData::new(seat),
+            state: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct PointerState {
+    active_surface_id: Option<ObjectId>,
+    // TODO: drag_and_drop: DragAndDrop,
+    serial: u32,
+}
+
+impl PointerDataExt for PointerUserData {
+    fn pointer_data(&self) -> &PointerData {
+        &self.pdata
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct PendingMouse {
+    window_id: usize,
+    // TODO: copy_and_paste: Arc<Mutex<CopyAndPaste>>,
+    surface_coords: Option<(f64, f64)>,
+    button: Vec<(MousePress, ButtonState)>,
+    scroll: Option<(f64, f64)>,
+    in_window: bool,
+}
+
+impl PendingMouse {
+    // TODO: copy and paste
+    pub(super) fn create(window_id: usize) -> Arc<Mutex<Self>> {
+        Arc::new(Mutex::new(Self {
+            window_id,
+            button: vec![],
+            scroll: None,
+            surface_coords: None,
+            in_window: false,
+        }))
+    }
+
+    pub(super) fn queue(&mut self, evt: &PointerEvent) -> bool {
+        match evt.kind {
+            PointerEventKind::Enter { .. } => {
+                self.in_window = true;
+                false
+            }
+            PointerEventKind::Leave { .. } => {
+                let changed = self.in_window;
+                self.surface_coords = None;
+                self.in_window = false;
+                changed
+            }
+            PointerEventKind::Motion { .. } => {
+                let changed = self.surface_coords.is_none();
+                self.surface_coords.replace(evt.position);
+                changed
+            }
+            PointerEventKind::Press { button, .. } => {
+                fn linux_button(b: u32) -> Option<MousePress> {
+                    // See BTN_LEFT and friends in <linux/input-event-codes.h>
+                    match b {
+                        0x110 => Some(MousePress::Left),
+                        0x111 => Some(MousePress::Right),
+                        0x112 => Some(MousePress::Middle),
+                        _ => None,
+                    }
+                }
+                let button = match linux_button(button) {
+                    Some(button) => button,
+                    None => return false,
+                };
+                let changed = self.button.is_empty();
+                self.button.push((button, ButtonState::Pressed));
+                changed
+            }
+            PointerEventKind::Axis {
+                horizontal,
+                vertical,
+                ..
+            } => {
+                let changed = self.scroll.is_none();
+                let (x, y) = self.scroll.take().unwrap_or((0., 0.));
+                self.scroll
+                    .replace((x + horizontal.absolute, y + vertical.absolute));
+                changed
+            }
+            _ => false,
+        }
+    }
+
+    pub(super) fn next_button(pending: &Arc<Mutex<Self>>) -> Option<(MousePress, ButtonState)> {
+        let mut pending = pending.lock().unwrap();
+        if pending.button.is_empty() {
+            None
+        } else {
+            Some(pending.button.remove(0))
+        }
+    }
+
+    pub(super) fn coords(pending: &Arc<Mutex<Self>>) -> Option<(f64, f64)> {
+        pending.lock().unwrap().surface_coords.take()
+    }
+
+    pub(super) fn scroll(pending: &Arc<Mutex<Self>>) -> Option<(f64, f64)> {
+        pending.lock().unwrap().scroll.take()
+    }
+
+    pub(super) fn in_window(pending: &Arc<Mutex<Self>>) -> bool {
+        pending.lock().unwrap().in_window
+    }
+}
 
 fn event_serial(event: &PointerEvent) -> Option<u32> {
     Some(match event.kind {
