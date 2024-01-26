2a97b72be55d5311c6d8fee2dd33483ae71f399f
ErrorNoInternet <errornointernet@envs.net>
2024-01-26 16:25:21 +0800

    chore(nix): update hashes for git
    It is no longer fetched from git.

--- nix/flake.nix
+++ nix/flake.nix
@@ -98,8 +98,7 @@
         cargoLock = {
           lockFile = ../Cargo.lock;
           outputHashes = {
-            "xcb-imdkit-0.2.0" = "sha256-L+NKD0rsCk9bFABQF4FZi9YoqBHr4VAZeKAWgsaAegw=";
-            "xcb-1.2.1" = "sha256-zkuW5ATix3WXBAj2hzum1MJ5JTX3+uVQ01R1vL6F1rY=";
+            "xcb-imdkit-0.3.0" = "sha256-fTpJ6uNhjmCWv7dZqVgYuS2Uic36XNYTbqlaly5QBjI=";
           };
         };
 
