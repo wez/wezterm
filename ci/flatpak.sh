1e27cded2feca1c73029dbda332987a6ce8be773
Wez Furlong <wez@wezfurlong.org>
2024-01-27 12:46:31 -0700

    update flatpak runtime version
    Looking at this because the build failed in the latest release:
    https://github.com/flathub/org.wezfurlong.wezterm/pull/16

--- ci/flatpak.sh
+++ ci/flatpak.sh
@@ -2,10 +2,13 @@
 set -xe
 
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install --noninteractive --user flathub org.freedesktop.Platform//21.08 org.freedesktop.Sdk//21.08 org.freedesktop.Sdk.Extension.rust-stable//21.08
+flatpak install --noninteractive --user flathub org.freedesktop.Platform//23.08 org.freedesktop.Sdk//23.08 org.freedesktop.Sdk.Extension.rust-stable//23.08
 
 flatpak install --noninteractive --user org.freedesktop.appstream-glib
-flatpak run --env=G_DEBUG=fatal-criticals org.freedesktop.appstream-glib validate assets/wezterm.appdata.xml
+
+# Disabled for now: seems like it has an OpenSSL problem and fails to use SSL when
+# validating the screenshot URLs
+#flatpak run --env=G_DEBUG=fatal-criticals org.freedesktop.appstream-glib validate assets/wezterm.appdata.xml
 
 python3 -m pip install toml aiohttp
 curl -L 'https://github.com/flatpak/flatpak-builder-tools/raw/master/cargo/flatpak-cargo-generator.py' > /tmp/flatpak-cargo-generator.py
