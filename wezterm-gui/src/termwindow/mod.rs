wez/wezterm

commit a0974a2537546518b3ff181329daf0185188c1aa

Implement SetInnerSizeCompleted events
These are used to signal, after a set_inner_size() call, at what point
we can expect any and all of its related Resized events to have already
been dispatched.  SetInnerSizeCompleted events are currently used to fix
a race condition in rescaling the terminal in which the number of cells
of the terminal (e.g., 80x24) can change when quickly rescaling the
terminal.

wezterm-gui/src/termwindow/mod.rs
@@ -54,7 +54,7 @@ use mux_lua::MuxPane;
 use smol::channel::Sender;
 use smol::Timer;
 use std::cell::{RefCell, RefMut};
-use std::collections::HashMap;
+use std::collections::{HashMap, LinkedList};
 use std::ops::Add;
 use std::rc::Rc;
 use std::sync::atomic::{AtomicBool, Ordering};
@@ -145,6 +145,10 @@ pub enum TermWindowNotif {
     EmitStatusUpdate,
     Apply(Box<dyn FnOnce(&mut TermWindow) + Send + Sync>),
     SwitchToMuxWindow(MuxWindowId),
+    SetInnerSize {
+        width: usize,
+        height: usize,
+    },
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -365,6 +369,8 @@ pub struct TermWindow {
     /// Window dimensions and dpi
     pub dimensions: Dimensions,
     pub window_state: WindowState,
+    pub resizes_pending: usize,
+    pending_scale_changes: LinkedList<resize::ScaleChange>,
     /// Terminal dimensions
     terminal_size: TerminalSize,
     pub mux_window_id: MuxWindowId,
@@ -689,6 +695,8 @@ impl TermWindow {
             render_metrics,
             dimensions,
             window_state: WindowState::default(),
+            resizes_pending: 0,
+            pending_scale_changes: LinkedList::new(),
             terminal_size,
             render_state,
             input_map: InputMap::new(&config),
@@ -942,6 +950,11 @@ impl TermWindow {
                 live_resizing,
             } => {
                 self.resize(dimensions, window_state, window, live_resizing);
+                Ok(true)
+            }
+            WindowEvent::SetInnerSizeCompleted => {
+                self.resizes_pending -= 1;
+                self.apply_pending_scale_changes();
                 Ok(true)
             }
             WindowEvent::AdviseModifiersLedStatus(modifiers, leds) => {
@@ -1287,9 +1300,19 @@ impl TermWindow {
                 self.update_title();
                 window.invalidate();
             }
+            TermWindowNotif::SetInnerSize { width, height } => {
+                self.set_inner_size(width, height);
+            }
         }
 
         Ok(())
+    }
+
+    fn set_inner_size(&mut self, width: usize, height: usize) {
+        if let Some(window) = &self.window {
+            self.resizes_pending += 1;
+            window.set_inner_size(width, height);
+        }
     }
 
     /// Take care to remove our panes from the mux, otherwise
@@ -2579,21 +2602,9 @@ impl TermWindow {
                 self.activate_tab_relative(*n, false)?;
             }
             ActivateLastTab => self.activate_last_tab()?,
-            DecreaseFontSize => {
-                if let Some(w) = window.as_ref() {
-                    self.decrease_font_size(w)
-                }
-            }
-            IncreaseFontSize => {
-                if let Some(w) = window.as_ref() {
-                    self.increase_font_size(w)
-                }
-            }
-            ResetFontSize => {
-                if let Some(w) = window.as_ref() {
-                    self.reset_font_size(w)
-                }
-            }
+            DecreaseFontSize => self.decrease_font_size(),
+            IncreaseFontSize => self.increase_font_size(),
+            ResetFontSize => self.reset_font_size(),
             ResetFontAndWindowSize => {
                 if let Some(w) = window.as_ref() {
                     self.reset_font_and_window_size(&w)?

wezterm-gui/src/termwindow/resize.rs
@@ -13,6 +13,12 @@ pub struct RowsAndCols {
     pub cols: usize,
 }
 
+#[derive(Debug)]
+pub enum ScaleChange {
+    Absolute(f64),
+    Relative(f64),
+}
+
 impl super::TermWindow {
     pub fn resize(
         &mut self,
@@ -62,6 +68,24 @@ impl super::TermWindow {
             modal.reconfigure(self);
         }
         self.emit_window_event("window-resized", None);
+    }
+
+    pub fn apply_pending_scale_changes(&mut self) {
+        while self.resizes_pending == 0 {
+            match self.pending_scale_changes.pop_front() {
+                Some(ScaleChange::Relative(change)) => {
+                    if let Some(window) = self.window.as_ref().map(|w| w.clone()) {
+                        self.adjust_font_scale(self.fonts.get_font_scale() * change, &window);
+                    }
+                }
+                Some(ScaleChange::Absolute(change)) => {
+                    if let Some(window) = self.window.as_ref().map(|w| w.clone()) {
+                        self.adjust_font_scale(change, &window);
+                    }
+                }
+                None => break,
+            }
+        }
     }
 
     pub fn apply_scale_change(&mut self, dimensions: &Dimensions, font_scale: f64) {
@@ -313,7 +337,7 @@ impl super::TermWindow {
                 // pixel geometry which is considered to be a user-driven resize.
                 // Stashing the dimensions here avoids that misconception.
                 self.dimensions = dims;
-                window.set_inner_size(dims.pixel_width, dims.pixel_height);
+                self.set_inner_size(dims.pixel_width, dims.pixel_height);
             }
         }
     }
@@ -430,16 +454,22 @@ impl super::TermWindow {
         }
     }
 
-    pub fn decrease_font_size(&mut self, window: &Window) {
-        self.adjust_font_scale(self.fonts.get_font_scale() / 1.1, window);
+    pub fn decrease_font_size(&mut self) {
+        self.pending_scale_changes
+            .push_back(ScaleChange::Relative(1.0 / 1.1));
+        self.apply_pending_scale_changes();
     }
 
-    pub fn increase_font_size(&mut self, window: &Window) {
-        self.adjust_font_scale(self.fonts.get_font_scale() * 1.1, window);
+    pub fn increase_font_size(&mut self) {
+        self.pending_scale_changes
+            .push_back(ScaleChange::Relative(1.1));
+        self.apply_pending_scale_changes();
     }
 
-    pub fn reset_font_size(&mut self, window: &Window) {
-        self.adjust_font_scale(1.0, window);
+    pub fn reset_font_size(&mut self) {
+        self.pending_scale_changes
+            .push_back(ScaleChange::Absolute(1.0));
+        self.apply_pending_scale_changes();
     }
 
     pub fn set_window_size(&mut self, size: TerminalSize, window: &Window) -> anyhow::Result<()> {

wezterm-gui/src/scripting/guiwin.rs
@@ -56,7 +56,8 @@ impl UserData for GuiWin {
         methods.add_method(
             "set_inner_size",
             |_, this, (width, height): (usize, usize)| {
-                this.window.set_inner_size(width, height);
+                this.window
+                    .notify(TermWindowNotif::SetInnerSize { width, height });
                 Ok(())
             },
         );

window/examples/async.rs
@@ -86,7 +86,8 @@ impl MyWindow {
             | WindowEvent::DraggedFile(_)
             | WindowEvent::DroppedFile(_)
             | WindowEvent::PerformKeyAssignment(_)
-            | WindowEvent::MouseLeave => {}
+            | WindowEvent::MouseLeave
+            | WindowEvent::SetInnerSizeCompleted => {}
         }
     }
 }

window/src/lib.rs
@@ -171,6 +171,9 @@ pub enum WindowEvent {
         live_resizing: bool,
     },
 
+    /// Called when a program-requested set_inner_size() has finished
+    SetInnerSizeCompleted,
+
     /// Called when the window has been invalidated and needs to
     /// be repainted
     NeedRepaint,

window/src/os/macos/window.rs
@@ -776,6 +776,13 @@ impl WindowOps for Window {
     fn set_inner_size(&self, width: usize, height: usize) {
         Connection::with_window_inner(self.id, move |inner| {
             inner.set_inner_size(width, height);
+            if let Some(window_view) = WindowView::get_this(unsafe { &**inner.view }) {
+                window_view
+                    .inner
+                    .borrow_mut()
+                    .events
+                    .dispatch(WindowEvent::SetInnerSizeCompleted);
+            }
             Ok(())
         });
     }

window/src/os/wayland/window.rs
@@ -976,7 +976,7 @@ impl WaylandWindowInner {
         Ok(())
     }
 
-    fn set_inner_size(&mut self, width: usize, height: usize) -> Dimensions {
+    fn set_inner_size(&mut self, width: usize, height: usize) {
         let pixel_width = width as i32;
         let pixel_height = height as i32;
         let surface_width = self.pixels_to_surface(pixel_width) as u32;
@@ -993,7 +993,7 @@ impl WaylandWindowInner {
         // apply the synthetic configure event to the inner surfaces
         self.dispatch_pending_event();
 
-        self.dimensions.clone()
+        self.events.dispatch(WindowEvent::SetInnerSizeCompleted);
     }
 
     fn do_paint(&mut self) -> anyhow::Result<()> {

window/src/os/windows/window.rs
@@ -898,6 +898,10 @@ impl WindowOps for Window {
                             SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOZORDER,
                         );
                         wm_paint(hwnd.0, 0, 0, 0);
+                        if let Some(inner) = rc_from_hwnd(hwnd.0) {
+                            let mut inner = inner.borrow_mut();
+                            inner.events.dispatch(WindowEvent::SetInnerSizeCompleted);
+                        }
                     }
                 } else {
                     log::trace!(

commit dd20727ba6ee2b7401592b7ac9724ccd8094617b

gui: pass window to set_inner_size()
This fixes a regression from a0974a25 where TermWindow::set_inner_size()
can be called before the TermWindow's window has been set.

wezterm-gui/src/termwindow/mod.rs
@@ -1301,18 +1301,16 @@ impl TermWindow {
                 window.invalidate();
             }
             TermWindowNotif::SetInnerSize { width, height } => {
-                self.set_inner_size(width, height);
+                self.set_inner_size(window, width, height);
             }
         }
 
         Ok(())
     }
 
-    fn set_inner_size(&mut self, width: usize, height: usize) {
-        if let Some(window) = &self.window {
-            self.resizes_pending += 1;
-            window.set_inner_size(width, height);
-        }
+    fn set_inner_size(&mut self, window: &Window, width: usize, height: usize) {
+        self.resizes_pending += 1;
+        window.set_inner_size(width, height);
     }
 
     /// Take care to remove our panes from the mux, otherwise

wezterm-gui/src/termwindow/resize.rs
@@ -337,7 +337,7 @@ impl super::TermWindow {
                 // pixel geometry which is considered to be a user-driven resize.
                 // Stashing the dimensions here avoids that misconception.
                 self.dimensions = dims;
-                self.set_inner_size(dims.pixel_width, dims.pixel_height);
+                self.set_inner_size(window, dims.pixel_width, dims.pixel_height);
             }
         }
     }