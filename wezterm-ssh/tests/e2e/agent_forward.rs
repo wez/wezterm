6ad0da98d8371d34dfa6c8018ae4a022ecdc6470
Wez Furlong <wez@wezfurlong.org>
2024-05-13 11:43:10 -0700

    deps: remove smol-potat, update futures-lite
    smol-potat hasn't had updates in 4 years and was bifurcating some
    big deps.  It wasn't necessary; it was trivially easy to replace
    with smol::block_on with no meaningful increase in boilerplate,
    and the result is much more understandable in purpose and effect.

--- wezterm-ssh/tests/e2e/agent_forward.rs
+++ wezterm-ssh/tests/e2e/agent_forward.rs
@@ -14,40 +14,42 @@ async fn session_with_agent_forward(
 }
 
 #[rstest]
-#[smol_potat::test]
 #[cfg_attr(not(any(target_os = "macos", target_os = "linux")), ignore)]
 #[cfg_attr(not(feature = "libssh-rs"), ignore)]
-async fn ssh_add_should_be_able_to_list_identities_with_agent_forward(
+fn ssh_add_should_be_able_to_list_identities_with_agent_forward(
     #[future] session_with_agent_forward: SessionWithSshd,
 ) {
-    let session: SessionWithSshd = session_with_agent_forward.await;
+    smol::block_on(async {
+        let session: SessionWithSshd = session_with_agent_forward.await;
 
-    let (pty, _child_process) = session
-        .request_pty("dumb", PtySize::default(), Some("ssh-add -l"), None)
-        .await
-        .unwrap();
-    let mut reader = pty.try_clone_reader().unwrap();
-    let mut output: String = String::new();
-    reader.read_to_string(&mut output).unwrap();
-    assert_eq!(output, "The agent has no identities.\r\n");
+        let (pty, _child_process) = session
+            .request_pty("dumb", PtySize::default(), Some("ssh-add -l"), None)
+            .await
+            .unwrap();
+        let mut reader = pty.try_clone_reader().unwrap();
+        let mut output: String = String::new();
+        reader.read_to_string(&mut output).unwrap();
+        assert_eq!(output, "The agent has no identities.\r\n");
+    })
 }
 
 #[rstest]
-#[smol_potat::test]
 #[cfg_attr(not(any(target_os = "macos", target_os = "linux")), ignore)]
 #[cfg_attr(not(feature = "libssh-rs"), ignore)]
-async fn no_agent_forward_should_happen_when_disabled(#[future] session: SessionWithSshd) {
-    let session: SessionWithSshd = session.await;
+fn no_agent_forward_should_happen_when_disabled(#[future] session: SessionWithSshd) {
+    smol::block_on(async {
+        let session: SessionWithSshd = session.await;
 
-    let (pty, _child_process) = session
-        .request_pty("dumb", PtySize::default(), Some("ssh-add -l"), None)
-        .await
-        .unwrap();
-    let mut reader = pty.try_clone_reader().unwrap();
-    let mut output: String = String::new();
-    reader.read_to_string(&mut output).unwrap();
-    assert_eq!(
-        output,
-        "Could not open a connection to your authentication agent.\r\n"
-    );
+        let (pty, _child_process) = session
+            .request_pty("dumb", PtySize::default(), Some("ssh-add -l"), None)
+            .await
+            .unwrap();
+        let mut reader = pty.try_clone_reader().unwrap();
+        let mut output: String = String::new();
+        reader.read_to_string(&mut output).unwrap();
+        assert_eq!(
+            output,
+            "Could not open a connection to your authentication agent.\r\n"
+        );
+    })
 }
