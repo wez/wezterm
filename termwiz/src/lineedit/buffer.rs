76ce9b6db26c4f72f91464120c7189f52bde096d
Ken Chou <zhang-j@klab.com>
2024-06-15 20:29:01 +0800

    Revert "Fix crash when jumping to the end of the line."
    This reverts commit f5ca810515989640022b8eec807f959cc84ea926.

--- termwiz/src/lineedit/buffer.rs
+++ termwiz/src/lineedit/buffer.rs
@@ -174,7 +174,15 @@ impl LineEditBuffer {
                 position
             }
             Movement::StartOfLine => 0,
-            Movement::EndOfLine => self.line.len(),
+            Movement::EndOfLine => {
+                let mut cursor =
+                    GraphemeCursor::new(self.line.len().saturating_sub(1), self.line.len(), false);
+                if let Ok(Some(pos)) = cursor.next_boundary(&self.line, 0) {
+                    pos
+                } else {
+                    self.cursor
+                }
+            }
             Movement::None => self.cursor,
         }
     }
