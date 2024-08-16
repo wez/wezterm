c9eb1b4adcec00b840e10427ee764f52a67aa2ba
Aliaksandr Truš <alex.trush@proton.me>
2024-08-16 23:04:10 +0200

    fix(wayland): update size behavior for tiled window

--- window/src/os/wayland/window.rs
+++ window/src/os/wayland/window.rs
@@ -1226,6 +1226,7 @@ impl WaylandState {
             .window_by_id(window_id)
             .expect("Inner Window should exist");
 
+        let is_frame_hidden = window_inner.borrow().window_frame.is_hidden();
         let p = window_inner.borrow().pending_event.clone();
         let mut pending_event = p.lock().unwrap();
 
@@ -1258,20 +1259,20 @@ impl WaylandState {
                 if configure.state.contains(SCTKWindowState::FULLSCREEN) {
                     state |= WindowState::FULL_SCREEN;
                 }
-                let fs_bits = SCTKWindowState::MAXIMIZED
-                    | SCTKWindowState::TILED_LEFT
-                    | SCTKWindowState::TILED_RIGHT
-                    | SCTKWindowState::TILED_TOP
-                    | SCTKWindowState::TILED_BOTTOM;
-                if !((configure.state & fs_bits).is_empty()) {
+                if configure.state.contains(SCTKWindowState::MAXIMIZED) {
                     state |= WindowState::MAXIMIZED;
                 }
 
                 // For MAXIMIZED and FULL_SCREEN window configure contains Windowed size.
                 // Replacing it with Wayland suggested bounds.
-                if state.contains(WindowState::MAXIMIZED) || state.contains(WindowState::FULL_SCREEN) {
+                if state.intersects(WindowState::MAXIMIZED | WindowState::FULL_SCREEN) {
                     if let Some((w, h)) = configure.suggested_bounds {
                         pending_event.configure.replace((w, h));
+                    }
+                } else if configure.state.contains(SCTKWindowState::TILED_TOP | SCTKWindowState::TILED_BOTTOM) && is_frame_hidden {
+                    // Tiled window without borders will take exactly half of the screen.
+                    if let Some((w, h)) = configure.suggested_bounds {
+                        pending_event.configure.replace((w / 2, h));
                     }
                 }
                 log::debug!(
