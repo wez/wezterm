wez/wezterm

commit a115fc0df77091ada2497fde22bc5c013e860ecd

fix iterm2 image cursor position
refs: https://github.com/wez/wezterm/issues/3266

term/src/terminalstate/image.rs
@@ -196,7 +196,7 @@ impl TerminalState {
                 xpos += x_delta;
             }
             ypos += y_delta;
-            if !params.do_not_move_cursor {
+            if !params.do_not_move_cursor && y < height_in_cells - 1 {
                 self.new_line(false);
             }
         }
@@ -213,7 +213,7 @@ impl TerminalState {
             if bottom_right {
                 self.set_cursor_pos(
                     &Position::Relative(width_in_cells as i64),
-                    &Position::Relative(-1),
+                    &Position::Relative(0),
                 );
             }
         }

commit 6d45190826308fa51e13f3ec23cf886691cdb9d2

fix non-iTerm2 cursor position after sixel image
Fixes #6110.

It seems the problem was introduced by a115fc0 to fix issue #3266.

term/src/terminalstate/image.rs
@@ -217,7 +217,7 @@ impl TerminalState {
                 xpos += x_delta;
             }
             ypos += y_delta;
-            if !params.do_not_move_cursor && y < height_in_cells - 1 {
+            if !params.do_not_move_cursor && (params.style != ImageAttachStyle::Iterm || y < height_in_cells - 1) {
                 self.new_line(false);
             }
         }