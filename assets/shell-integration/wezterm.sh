wez/wezterm

commit 24702de74a958460e3b9af3fbf21327b7ba4a3e3

shell integration: check for saved PS1 value before using it
On bash it's possible to hit ^C before we remembered the PS1 which would
cause the shell to end up with an empty PS1, making it unclear that the
^C actually worked as there is no prompt left.

I'm not sure I understand the need to mess with the variable every time
(could just add the OSC codes if not already present and never remove
them?), but at least make it so we don't end up with an empty prompt
line by checking the saved value isn't empty.

assets/shell-integration/wezterm.sh
@@ -486,9 +486,12 @@ __wezterm_semantic_precmd() {
 }
 
 function __wezterm_semantic_preexec() {
-  # Restore the original PS1/PS2
-  PS1="$__wezterm_save_ps1"
-  PS2="$__wezterm_save_ps2"
+  # Restore the original PS1/PS2 if set
+  if [ -n "${__wezterm_save_ps1+1}" ]; then
+	  PS1=$__wezterm_save_ps1"
+	  PS2="$__wezterm_save_ps2"
+	  unset __wezterm_save_ps1
+  fi
   # Indicate that the command output begins here
   printf "\033]133;C;\007"
   __wezterm_semantic_precmd_executing=1

commit c76c6c63ddfc966795200a2ddd92b9047d5e4996

shell-integration: Fix syntax error due to missing double quote
This fixes a regression introduced in commit
24702de74a958460e3b9af3fbf21327b7ba4a3e3.

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

assets/shell-integration/wezterm.sh
@@ -488,7 +488,7 @@ __wezterm_semantic_precmd() {
 function __wezterm_semantic_preexec() {
   # Restore the original PS1/PS2 if set
   if [ -n "${__wezterm_save_ps1+1}" ]; then
-	  PS1=$__wezterm_save_ps1"
+	  PS1="$__wezterm_save_ps1"
 	  PS2="$__wezterm_save_ps2"
 	  unset __wezterm_save_ps1
   fi