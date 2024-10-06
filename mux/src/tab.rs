2e83b117ca391491280cb472343cf64aa08851ef
Eric Nelson <e82.eric@gmail.com>
2024-10-05 23:22:43 -0700

    Update so that cli activate-pane will show/hide the floating pane when the terminal window is not focused.

--- mux/src/tab.rs
+++ mux/src/tab.rs
@@ -1898,6 +1898,10 @@ impl TabInner {
             }
         }
 
+        if self.float_pane_visible {
+            self.set_float_pane_visibility(false);
+        }
+
         if self.zoomed.is_some() {
             if !configuration().unzoom_on_switch_pane {
                 return;
@@ -1925,6 +1929,9 @@ impl TabInner {
                 tab_id: self.id,
                 visible,
             });
+            if let Some(window_id) = mux.window_containing_tab(self.id) {
+                mux.notify(MuxNotification::WindowInvalidated(window_id));
+            }
         }
     }
 
