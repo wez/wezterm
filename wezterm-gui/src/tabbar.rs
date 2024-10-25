6a8b64d974cdb9c34a905856f483a069a11361a6
Nishant Joshi <nishant.joshi@juspay.in>
2024-10-25 11:12:01 +0530

    feat(tabbar): add support to align tabs to the right

--- wezterm-gui/src/tabbar.rs
+++ wezterm-gui/src/tabbar.rs
@@ -550,6 +550,8 @@ impl TabBarState {
             Self::integrated_title_buttons(mouse_x, &mut x, config, &mut items, &mut line, &colors);
         }
 
+        line.reverse_cells(SEQ_ZERO);
+
         Self { line, items }
     }
 
