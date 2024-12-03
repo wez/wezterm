be62eb1c60bf6ffab436c03dc67d6f8f984beb3e
occupyhabit <wangmengjiao@outlook.com>
2024-12-03 11:14:47 +0800

    chore: remove redundant words in comment
    Signed-off-by: occupyhabit <wangmengjiao@outlook.com>

--- window/src/os/windows/window.rs
+++ window/src/os/windows/window.rs
@@ -2565,7 +2565,7 @@ unsafe fn key(hwnd: HWND, msg: UINT, wparam: WPARAM, lparam: LPARAM) -> Option<L
         && (keys[VK_CONTROL as usize] & 0x80 != 0)
     {
         // AltGr is pressed; while AltGr is on the RHS of the keyboard
-        // is is not the same thing as right-alt.
+        // is not the same thing as right-alt.
         // Windows sets RMENU and CONTROL to indicate AltGr and we
         // have to keep these in the key state in order for ToUnicode
         // to map the key correctly.
